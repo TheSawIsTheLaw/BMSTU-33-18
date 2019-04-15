a2347fe83a8185ae700dd79df5a7148afb1bea30
Lemeshkin Bogdan <690-lba18u352@users.noreply.git.iu7.bmstu.ru>
2019-04-15 23:16:11 +0300

    Add print_battlefield

--- main.c
+++ main.c
@@ -43,7 +43,16 @@ int copy_battlefield(char from[][3], char to[][3])
 
 void print_battlefield(char bf[][3])
 {
-
+    printf("┏━━━┳━━━┳━━━┓\n");
+    for (int i=0; i<2 ; i++)
+    {
+        for(int j=0; j<3; j++)
+            printf("┃ %c%s",bf[i][j],(j+1) % 3 ? " ":" ┃\n" );
+        printf("┣━━━╋━━━╋━━━┫\n");
+    }
+    for(int j=0; j<3; j++)
+        printf("┃ %c%s",bf[2][j],(j+1) % 3 ? " ":" ┃\n" );
+    printf("┗━━━┻━━━┻━━━┛\n");
 }
 
 
