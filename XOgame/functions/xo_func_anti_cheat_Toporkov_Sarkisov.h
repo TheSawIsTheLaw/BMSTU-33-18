37b8cdd94ff9b1779cdb3a78725dc1c952cd3b4d
Paw.toporkov <pawtoporkov@MacBook-Pro-Pawtoporkov.local>
2019-04-18 18:30:02 +0300

    add anti_cheat

---
+++ XOgame/functions/xo_func_anti_cheat_Toporkov_Sarkisov.h
@@ -0,0 +1,42 @@
+int anti_cheat(char before[][3], char after[][3])
+{
+    int x_cord[9];
+    int y_cord[9];
+    int n_x_y = 0;
+    int cur = 0;
+    int N = 3;
+    
+    // находим заполненные клетки до последнего хода
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+            if (before[i][j] == 'X' || before[i][j] == 'O')
+            {
+                x_cord[n_x_y] = i;
+                y_cord[n_x_y] = j;
+                n_x_y++;
+            }
+    }
+    
+    // находим кол-во заполненных клеток после последнего хода, а также проверяем, что новый ход корректен
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (after[i][j] == 'X' || after[i][j] == 'O')
+                cur++;
+            if (after[i][j] != 'X' && after[i][j] != 'O' && after[i][j] != ' ')
+                return 1;
+        }
+    }
+    
+    // проверям, что был сделан только один ход
+    if (cur - n_x_y != 1)
+        return 1;
+    
+    // проверяем, что ранне введенные данные не были изменены
+    for (int i = 0; i < n_x_y; i++)
+        if (before[x_cord[i]][y_cord[i]] != after[x_cord[i]][y_cord[i]])
+            return 1;
+    return 0;
+}
