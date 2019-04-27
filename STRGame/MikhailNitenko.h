95cbbef683a0c801fc64d17a1ee7570b1a08ce6a
nmyu18u413 <mnitenko@gmail.com>
2019-04-27 11:56:03 +0300

    add begginings of functions
    Former-commit-id: 13686222cfdf74651e7b1827cb3a5cffbbc12a90

---
+++ STRGame/MikhailNitenko.h
@@ -0,0 +1,18 @@
+int strlen(const char *s)
+{
+    int l = 0;
+    while(*l++ != '\0')
+
+    return l;
+}
+
+char* 
+strtok(char* str, const char* delimiters)
+{
+    int i = 0;
+    int 
+    while (1)
+    {
+        if 
+    }
+}
