ba346ec6cf665eef991bca0f505ea570570f53c4
Kononenko Sergey <sergkononenko@mail.ru>
2019-04-27 13:36:59 +0300

    Add template for SergeyKononenko.h header
    Former-commit-id: 698de2c8a410c00fda2d82782a21696d2d352fe9

---
+++ STRGame/SergeyKononenko.h
@@ -0,0 +1,27 @@
+/* Sergey Kononenko header file best lead */
+
+#define ever ;;
+
+int my_strlen(const char *s)
+{
+    int l = 0;
+    while(*s++ != '\0')
+
+        return l;
+}
+
+char* my_strsplit(const char* src_array, char* split_array)
+{
+    for(ever)
+    {
+        //
+    }
+}
+
+char* my_strtok(char* str, const char* delimiters)
+{
+    for(ever)
+    {
+        //
+    }
+}
