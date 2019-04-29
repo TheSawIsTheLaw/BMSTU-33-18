cf69719deccbdacec7b6314116abe2a129a09bb3
Pavel Perestoronin <p_perestornin@mail.ru>
2019-04-30 00:25:17 +0300

    made my func universal
    Former-commit-id: 87ff590e7fe14899503beb4c49c9523a0496be89

--- XOgame/strategies/xo_ai_perestoronin.h
+++ XOgame/strategies/xo_ai_perestoronin.h
@@ -1,83 +1,168 @@
 #define FIELD_SIZE 9
+#define SIDE_SIZE 3
+#define MADE_SHOT 0
+#define NOT_FAST 0
+#define NO_WAY 0
+#define GOOD_WAY 1
 
-
-int fast_decision(char pos_1, char pos_2, char pos_3)
+int fast_decision(const char* const pos)
 {
     int x = 0, o = 0; empty = 0;
+    for (int i = 0; i < SIDE_SIZE; i++)
+    {
+        if (pos[i] == 'X')
+            x++;
+        if (pos[i] == 'O') 
+            o++;
+        if (pos[i] == ' ')
+            empty = i + 1;
+    }
 
-    if (pos_1 == 'X') x++;
-    if (pos_2 == 'X') x++;
-    if (pos_3 == 'X') x++;
-
-    if (pos_1 == 'O') o++;
-    if (pos_2 == 'O') o++;
-    if (pos_3 == 'O') o++;
-
-    if (pos_1 == ' ') empty = 1;
-    if (pos_2 == ' ') empty = 2;
-    if (pos_3 == ' ') empty = 3;
-
-    return (x == 2 || o == 2) ? empty : 0;
+    return (x == SIDE_SIZE - 1 || o == SIDE_SIZE - 1) ? empty : NOT_FAST;
 }
 
 
-int space_counter(char BF[][3], int array[])
+int space_counter(char BF[][SIDE_SIZE], int *array)
 {
-    counter = 0, int k = 0;
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
+    int counter = 0, k = 0;
+    for (int i = 0; i < SIDE_SIZE; i++)
+        for (int j = 0; j < SIDE_SIZE; j++)
             if (BF[i][j] == ' ')
             {
                 counter++;
-                array[k++] = i * 3 + j;
+                array[k++] = i * SIDE_SIZE + j;
             } 
     return counter;
 }
 
 
-int make_shot_perestoronin(char symb, char BF[][3])
+void column_array_maker(char BF[][SIDE_SIZE], char *col_arr, const int column)
+{
+    for (int i = 0; i < SIDE_SIZE; i++)
+        col_arr[i] = BF[i][column];
+}
+
+
+void diag_array_maker(char BF[][SIDE_SIZE], char *col_arr)
+{
+    for (int i = 0; i < SIDE_SIZE; i++)
+        col_arr[i] = BF[i][i];
+}
+
+
+void rev_diag_array_maker(char BF[][SIDE_SIZE], char *col_arr)
+{
+    for (int i = 0; i < SIDE_SIZE; i++)
+        col_arr[i] = BF[i][SIDE_SIZE - 1 - i];
+}
+
+
+int check_row(char BF[][SIDE_SIZE], const int row, const char unsymb)
+{
+    for (int i = 0; i < SIDE_SIZE; i++)
+    {
+        if (BF[row][i] == unsymb)
+            return NO_WAY;
+    }
+    return GOOD_WAY;
+}
+
+
+int check_column(char BF[][SIDE_SIZE], const int column, const char unsymb)
+{
+    for (int i = 0; i < SIDE_SIZE; i++)
+    {
+        if (BF[i][column] == unsymb)
+            return NO_WAY;
+    }
+    return GOOD_WAY;
+}
+
+
+int check_diag(char BF[][SIDE_SIZE], const char unsymb)
+{
+    for (int i = 0; i < SIDE_SIZE; i++)
+    {
+        if (BF[i][i] == unsymb)
+            return NO_WAY;
+    }
+    return GOOD_WAY;
+}
+
+
+int check_rev_diag(char BF[][SIDE_SIZE], const char unsymb)
+{
+    for (int i = 0; i < SIDE_SIZE; i++)
+    {
+        if (BF[i][SIDE_SIZE - 1 - i] == unsymb)
+            return NO_WAY;
+    }
+    return GOOD_WAY;
+}
+
+
+int make_shot_perestoronin(const char symb, char BF[][SIDE_SIZE])
 {
     int i, j, unsymb;
     int free_slots[FIELD_SIZE];
     int space_num = space_counter(BF, free_slots);
-    
+    int work_arr[SIDE_SIZE];
     unsymb = (symb == 'X') ? 'O' : 'X';
-    if (space_num == FIELD_SIZE) 
+
+    if (BF[SIDE_SIZE / 2][SIDE_SIZE / 2] == ' ') 
     {
-        BF[0][0] = symb;
-        return 0;
+        BF[SIDE_SIZE / 2][SIDE_SIZE / 2] = symb;
+        return MADE_SHOT;
     }
 
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < SIDE_SIZE; i++)
     {
-        j = fast_decision(BF[i][0], BF[i][1], BF[i][2]);
+        j = fast_decision(BF[i]);
         if (j != 0) 
         {
             BF[i][j - 1] = symb;
-            return 0;
+            return MADE_SHOT;
         }
-        j = fast_decision(BF[0][i], BF[1][i], BF[2][i]);
+        column_array_maker(BF, work_arr, i);
+        j = fast_decision(work_arr);
         if (j != 0) 
         {
             BF[j - 1][i] = symb;
-            return 0;
+            return MADE_SHOT;
         }
     }
-    j = fast_decision(a[0][0], a[1][1], a[2][2]);
-    if (j-- != 0) 
+
+    diag_array_maker(BF, work_arr);
+    j = fast_decision(work_arr);
+    if (j-- != 0)
     {
         BF[j][j] = symb;
-        return 0;
+        return MADE_SHOT;
     }
-    j = fast_decision(a[0][2], a[1][1], a[2][0]);
+
+    rev_diag_array_maker(BF, work_arr);
+    j = fast_decision(work_arr);
     if (j-- != 0) 
     {
-        BF[j][2 - j] = symb;
-        return 0;
+        BF[j][SIDE_SIZE - j - 1] = symb;
+        return MADE_SHOT;
+    }
+
+    for (int i = 0; i < FIELD_SIZE; i++)
+    {
+        if (BF[i / SIDE_SIZE][i % SIDE_SIZE] == ' ')
+            if (check_column(BF, i % SIDE_SIZE, unsymb) ||
+                check_row(BF, i / SIDE_SIZE, unsymb) ||
+                (i / SIDE_SIZE == i % SIDE_SIZE && check_diag(BF, unsymb)) ||
+                (i / SIDE_SIZE == SIDE_SIZE - 1 - (i / SIDE_SIZE) && check_rev_diag(BF, unsymb)))
+                {
+                    BF[i / SIDE_SIZE][i % SIDE_SIZE] = symb;
+                    return MADE_SHOT;
+                }
     }
 
     i = rand() % space_num;
     j = free_slots[i];
-    BF[j / 3][j % 3] = symb;
-    return 0;
+    BF[j / SIDE_SIZE][j % SIDE_SIZE] = symb;
+    return MADE_SHOT;
 }
