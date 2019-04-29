d064967ffe75694730d4145d8a28a896816fd523
Romanov Alexey <user-lab01@LAB-01-18.vlab.bmstu.ru>
2019-04-29 14:51:50 +0300

    include in main.c all files + update readme
    Former-commit-id: f912760b1b8128b54440fbec628fbd166181d0ee

--- STRGame/main.c
+++ STRGame/main.c
@@ -1,4 +1,42 @@
+/* Файл main.c - не трогать, если хотите что-то протестировать
+   клоньте себе на компьютер и тестируйте */
+
 #include <stdio.h>
+#include <time.h>
+/* #include <AlisSukocheva.h>
+#include <AnastasiiaNamestnik.h>
+#include <ArtemSarkisov.h>
+#include <BogdanLemeshkin.h>
+#include <DmitryKovalev.h>
+#include <EmilSimonenko.h>
+#include <IlyaChelyadinov.h>
+#include <KarimAkhmetov.h>
+#include <LyuvobProkhorova.h>
+#include <MikhailNitenko.h>
+#include <NadezhdaAksenova.h>
+#include <NikitaBurtelo#include <AlisSukocheva.h>
+#include <AnastasiiaNamestnik.h>
+#include <ArtemSarkisov.h>
+#include <BogdanLemeshkin.h>
+#include <DmitryKovalev.h>
+#include <EmilSimonenko.h>
+#include <IlyaChelyadinov.h>
+#include <KarimAkhmetov.h>
+#include <LyuvobProkhorova.h>
+#include <MikhailNitenko.h>
+#include <NadezhdaAksenova.h>
+#include <NikitaBurtelov.h>
+#include <PavelPerestoronin.h>
+#include <PavelToporkov.h>
+#include <SergeyKononenko.h>
+#include <SergeyMinenko.h>
+#include <SergeySaburov.h>
+#include <SergeySverdlov.h>
+#include <VladKrivozubov.h>
+#include <VladislavChernenko.h>
+#include <VladislavGurishev.h>
+#include <YefimSokolov.h> */
+
 #define OK 0
 
 int main(void)
