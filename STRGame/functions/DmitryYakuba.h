33d35f593adc8778aae32cb8d11a054c8b4d7c53
Yakuba Dmitry <612-yadv18u603@users.noreply.git.iu7.bmstu.ru>
2019-05-02 01:59:35 +0300

    New split func from 666
    Former-commit-id: 13e3c74751a2a824f8b94d8d2edc5fabf90e077b

--- STRGame/functions/DmitryYakuba.h
+++ STRGame/functions/DmitryYakuba.h
@@ -1,12 +1,33 @@
-// N - максимальная длина строки, она будет объявлена в мейне
+#define Z 0
+#define IZ '\0'
+
+typedef int I;
 
 char *strtok_Yakuba(char *string, const char *delim)
 {
     return 0;
 }
 
-int split_Yakuba(const char *string, char matrix[][N], const char symbol)
+int split_Yakuba(const char *s, char m[][N], const char sy)
 {
-    return 0;
+    I i = Z;
+    I r = Z;
+    I c = Z;
+    while (s[i])
+    {
+        if (sy == s[i])
+        {
+            m[r][c] = IZ;
+            ++r;
+            c = Z;
+        }
+        else
+        {
+            m[r][c] = s[i];
+            ++c;
+        }
+        ++i;
+    }
+    m[r][c] = IZ;
+    return ++r;
 }
-
