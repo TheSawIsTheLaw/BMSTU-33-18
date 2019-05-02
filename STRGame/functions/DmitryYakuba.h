7cf531e6f9d2f9c3c2a1572bd2bf9cf4cd4171d7
Yakuba Dmitry <612-yadv18u603@users.noreply.git.iu7.bmstu.ru>
2019-05-02 12:58:44 +0300

    Strtok_Yakuba was added. Where are tests? Hail Satan!
    Former-commit-id: 1ab60929dfe6be19db2f0c9fd8abe490328c24b9

--- STRGame/functions/DmitryYakuba.h
+++ STRGame/functions/DmitryYakuba.h
@@ -1,11 +1,57 @@
 #define Z 0
 #define IZ '\0'
+#define ONE 1
 
 typedef int I;
 
+char *pre;
+
 char *strtok_Yakuba(char *string, const char *delim)
 {
-    return 0;
+    if (string != NULL)
+    {
+
+        int i = Z, q = Z;
+        while (string[i])
+        {
+            while (delim[q])
+            {
+                if (delim[q] == string[i])
+                {
+                    string[i] = IZ;
+                    pre = &string[i+ONE];
+                    return string;
+                }
+                ++q;
+            }
+            ++i;
+            q = Z;
+        }
+        return NULL;
+    }
+    else
+    {
+        int i = Z, q = Z;
+        char* prepr = pre;
+        while (pre[i])
+        {
+            while (delim[q])
+            {
+                if (delim[q] == pre[i])
+                {
+                    pre[i] = IZ;
+                    pre = &pre[i+ONE];
+                    return prepr;
+                }
+                ++q;
+            }
+            ++i;
+            q = Z;
+        }
+        prepr = pre;
+        pre = NULL;
+        return prepr;
+    }
 }
 
 int split_Yakuba(const char *s, char m[][N], const char sy)
