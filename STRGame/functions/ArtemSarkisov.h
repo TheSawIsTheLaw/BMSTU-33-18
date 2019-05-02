60272a91373a5dbf59c175610663fe1e48d304d4
Sarkisov Artem <635-sas18u476@users.noreply.git.iu7.bmstu.ru>
2019-05-03 02:40:11 +0300

    ADD: strtok_Sarkisov() function. Former-commit-id: a32c87432201cf3c4525ea5a570914f73698950f

--- STRGame/functions/ArtemSarkisov.h
+++ STRGame/functions/ArtemSarkisov.h
@@ -2,7 +2,73 @@
 
 char *strtok_Sarkisov(char *string, const char *delim)
 {
-    return 0;
+    static char *stc;
+    static int len;
+    int num_s = 0;
+    int num_d = 0;
+    int len_str = 0;
+    int ind;
+
+    if (string != NULL)
+    {
+        stc = string;
+        while (string[len_str] != '\0')
+            len_str++;
+        len = len_str;
+    }
+
+    else if (len > 0)
+    {
+        stc++;
+        len--;
+        string = stc;
+    }
+
+    else
+    {
+        string = NULL;
+    }
+
+    while (*stc)
+    {
+        while (delim[num_d] != '\0')
+        {
+            if (stc[num_s] == delim[num_d])
+            {
+                num_s++;
+                num_d = 0;
+            }
+            else 
+                num_d++;
+        }
+
+        ind = num_s;
+        num_s = 0;
+        num_d = 0;
+
+        while (ind > 1)
+        {
+            *stc = '\0';
+            stc++;
+            len--;
+            ind--;
+        }
+
+        if (ind == 1)
+        {
+            *stc = '\0';
+            if (len > 1)
+            {
+                stc--;
+                len++;
+            }
+        }
+
+        stc++;
+        len--;
+    }
+
+    return string;
 }
 
 int split_Sarkisov(const char *string, char matrix[][N], const char symbol)
