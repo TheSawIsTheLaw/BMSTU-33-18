6cf2bc5f76fac76aa6e68cfc4e70b1c15fb7cc9a
Pavel Perestoronin <p_perestornin@mail.ru>
2019-05-02 20:24:07 +0300

    added strtok func
    Former-commit-id: 5d3287f903f7dc7f0a922f9fc484cc6fe4028975

--- STRGame/functions/PavelPerestoronin.h
+++ STRGame/functions/PavelPerestoronin.h
@@ -1,14 +1,51 @@
 // N - максимальная длина строки, она будет объявлена в мейне
 
+static char *olds;
 
 char *strtok_Perestoronin(char *string, const char *delim)
 {
-    return 0;
+	int i = 0, j = 0;
+	if (string == NULL)
+    {
+    	while (*olds)
+    	{
+    		while (delim[i])
+    		{
+    			if (*olds == delim[i])
+    			{
+    				*olds = '\0';
+    				return ++olds;
+    			}
+    			i++;
+    		}
+    		olds++;
+    	}
+    	return NULL;
+    }
+    else
+    {
+    	olds = string;
+    	while (string[i])
+    	{
+    		while (delim[j])
+    		{
+    			if (string[i] == delim[j])
+    			{
+    				string[i] = '\0';
+    				break; 
+    			}
+    			j++;
+    		}
+    		i++;
+    	}
+    	return olds;
+    }
+
 }
 
 int split_Perestoronin(const char *string, char matrix[][N], const char symbol)
 {
-	int i = 0, j = 0, k = 0;
+	register int i = 0, j = 0, k = 0;
 	while (string[k] != '\0')
 	{
 		if (string[k] == symbol)
@@ -23,4 +60,3 @@ int split_Perestoronin(const char *string, char matrix[][N], const char symbol)
     matrix[i][j] = '\0';
     return ++i;
 }
-
