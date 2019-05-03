63081611722acaba8e3919364611ad9cacdbcdeb
Toporkov Pavel <645-tpa18u531@users.noreply.git.iu7.bmstu.ru>
2019-05-03 20:01:05 +0300

    Update PavelToporkov.h
    Former-commit-id: a7e9894b166a892c2db685e7ff144ad41168b55d

--- STRGame/functions/PavelToporkov.h
+++ STRGame/functions/PavelToporkov.h
@@ -1,8 +1,47 @@
 // N - максимальная длина строки, она будет объявлена в мейне
 
+static char *old_str;
+
 char *strtok_Toporkov(char *string, const char *delim)
 {
-    return 0;
+    register int i = CONST_ZERO;
+    register int j = CONST_ZERO;
+    if (string != NULL)
+    {
+        old_str = string;
+        while (string[i])
+        {
+            while (delim[j])
+            {
+                if (delim[j] == string[i])
+                {
+                    string[i] = EX;
+                    break;
+                }
+                ++j;
+            }
+            ++i;
+        }
+        return old_str;
+    }
+    else
+    {
+        while (*old_str)
+        {
+            while (delim[i])
+            {
+                if (delim[i] == *old_str)
+                {
+                    *old_str = EX;
+                    ++old_str;
+                    return old_str;
+                }
+                ++i;
+            }
+            ++old_str;
+        }
+        return NULL;
+    }
 }
 
 int split_Toporkov(const char *string, char matrix[][N], const char symbol)
