a6e0a3cc9f013e0ee94bcbeaf15c6d3dc9844835
Saburov Sergey <661-ssm18u469@users.noreply.git.iu7.bmstu.ru>
2019-05-04 19:45:01 +0300

    Update SergeySaburov.h
    Former-commit-id: 9eee847b595a25cbb4ad3c069cbf0bc7684ea372

--- STRGame/functions/SergeySaburov.h
+++ STRGame/functions/SergeySaburov.h
@@ -7,6 +7,24 @@ char *strtok_Saburov(char *string, const char *delim)
 
 int split_Saburov(const char *string, char matrix[][N], const char symbol)
 {
-    return 0;
+    int i =0;
+    int j = 0;
+    int ind = 0;
+    while (string[ind] != '\0')
+    {
+        if (string[ind] == symbol)
+        {
+            matrix[i++][j] = '\0';
+            i++;
+            j = 0;
+        }
+        else
+        {
+            matrix[i][j] = string[ind];
+            j ++;
+        }
+        ind ++;
+    }
+    matrix[i][j] = '\0';
+    return i ++;
 }
-
