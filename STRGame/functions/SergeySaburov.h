5552e456198051be35839838e74d13d99277266f
Saburov Sergey <661-ssm18u469@users.noreply.git.iu7.bmstu.ru>
2019-05-05 20:16:17 +0300

    strtok function ADDED
    Former-commit-id: fe688f3b76d42ae693bd0c53eb3c968cc47ac348

--- STRGame/functions/SergeySaburov.h
+++ STRGame/functions/SergeySaburov.h
@@ -1,8 +1,45 @@
 // N - максимальная длина строки, она будет объявлена в мейне
+static char *late_s;
 
 char *strtok_Saburov(char *string, const char *delim)
 {
-    return 0;
+    register int i = 0;
+	register int j = 0;
+	if (string == NULL)
+    {
+    	while (*late_s)
+    	{
+    		while (delim[i])
+    		{
+    			if (*late_s == delim[i])
+                {
+                    *late_s = '\0';
+                     return ++late_s;
+                }
+    			i++;
+    		}
+    		late_s++;
+    	}
+    	return NULL;
+    }
+    else
+    {
+    	late_s = string;
+    	while (string[i])
+    	{
+    		while (delim[j])
+    		{
+                if(string[i] == delim[j])
+                {
+                    string[i] = '\0';
+                    break;
+                }
+    			j++;
+    		}
+    		i++;
+    	}
+    	return late_s;
+    }
 }
 
 int split_Saburov(const char *string, char matrix[][N], const char symbol)
