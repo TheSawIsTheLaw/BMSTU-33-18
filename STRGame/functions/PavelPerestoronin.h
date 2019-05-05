9c31bcfb6ad6b273a14f2cdaa6ddd5f17c9f9157
Pavel Perestoronin <p_perestornin@mail.ru>
2019-05-05 22:45:58 +0300

    changed strtok
    Former-commit-id: 4435106dd6d59cd0b44b3c37a686a834b1a2a531

--- STRGame/functions/PavelPerestoronin.h
+++ STRGame/functions/PavelPerestoronin.h
@@ -1,62 +1,48 @@
 // N - максимальная длина строки, она будет объявлена в мейне
-
 static char *olds;
 
 char *strtok_Perestoronin(char *string, const char *delim)
 {
-	int i = 0, j = 0;
-	if (string == NULL)
+    if (string == NULL)
     {
-    	while (*olds)
-    	{
-    		while (delim[i])
-    		{
-    			if (*olds == delim[i])
-    			{
-    				*olds = '\0';
-    				return ++olds;
-    			}
-    			i++;
-    		}
-    		olds++;
-    	}
-    	return NULL;
+        for (; *olds; olds++)
+            for (int i = 0; delim[i]; i++)
+                if (*olds == delim[i])
+                {
+                    *olds = '\0';
+                    return ++olds;
+                }
+        return NULL;
     }
     else
     {
-    	olds = string;
-    	while (string[i])
-    	{
-    		while (delim[j])
-    		{
-    			if (string[i] == delim[j])
-    			{
-    				string[i] = '\0';
-    				break; 
-    			}
-    			j++;
-    		}
-    		i++;
-    	}
-    	return olds;
+        olds = string;
+        for (int i = 0; string[i]; i++)
+            for (int j = 0; delim[j]; j++)
+                if (string[i] == delim[j])
+                {
+                    string[i] = '\0';
+                    break; 
+                }
+        return olds;
     }
 
 }
 
 int split_Perestoronin(const char *string, char matrix[][N], const char symbol)
 {
-	register int i = 0, j = 0, k = 0;
-	while (string[k] != '\0')
-	{
-		if (string[k] == symbol)
-		{
-			matrix[i++][j] = '\0';
-			j = 0;
-		}
-		else
-			matrix[i][j++] = string[k];
-		k++;
-	}
+    register int i = 0, j = 0, k = 0;
+    while (string[k] != '\0')
+    {
+        if (string[k] == symbol)
+        {
+            matrix[i++][j] = '\0';
+            j = 0;
+        }
+        else
+            matrix[i][j++] = string[k];
+        k++;
+    }
     matrix[i][j] = '\0';
     return ++i;
 }
