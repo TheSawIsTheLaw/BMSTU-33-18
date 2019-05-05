749a46db4b61f7fcca279fb6ab114803d9d8740a
Sverdlov Sergey <701-ssa18u822@users.noreply.git.iu7.bmstu.ru>
2019-05-05 21:57:00 +0300

    Add strtoc function SergeySverdlov.h
    Former-commit-id: 753759590bbbbcb5cb57de6d98cb26eeebd3e1fb

--- STRGame/functions/SergeySverdlov.h
+++ STRGame/functions/SergeySverdlov.h
@@ -2,7 +2,43 @@
 
 char *strtok_Sverdlov(char *string, const char *delim)
 {
-    return 0;
+    register int i = 0, j = 0;
+    static char *nach;
+    if (string != NULL)
+    {
+        nach = string;
+        while (string[i])
+        {
+            while (delim[j])
+            {
+                if (string[i] == delim[j])
+                {
+                    string[i] = '\0';
+                    break;
+                }
+                j ++;
+            }
+            i ++;
+        }
+        return nach;
+    }
+    else
+    {
+        while (*nach)
+        {
+            while (delim[j])
+            {
+                if (*nach == delim[j])
+                {
+                    string[i] = '\0';
+                    return ++nach;
+                }
+                j ++;
+            }
+            nach ++;
+        }
+        return NULL;
+    }
 }
 
 int split_Sverdlov(const char *string, char matrix[][N], const char symbol)
