aec618b5e04c5890c4d42eb7092e05e8526e3681
Namestnik Anastasiia <741-naa18u403@users.noreply.git.iu7.bmstu.ru>
2019-05-05 17:12:25 +0300

    Add Strtok function
    Former-commit-id: e3acb43b9e0b0381387a9e02e02d64aa6a4bafd1

--- STRGame/functions/AnastasiiaNamestnik.h
+++ STRGame/functions/AnastasiiaNamestnik.h
@@ -2,7 +2,44 @@
 
 char *strtok_Namestnik(char *string, const char *delim)
 {
-    return 0;
+    static char *initial;
+    register int i = 0, j = 0;
+    
+    if (string != NULL)
+    {
+        initial = string;
+        while (string[j])
+        {
+            while (delim[i])
+            {
+                if (string[j] == delim[i])
+                {
+                    string[j] = '\0';
+                    break;
+                }
+                i++;
+            }
+            j++;
+        }
+        return initial;
+    }
+    else
+    {
+        while (*initial)
+        {
+            while (delim[i])
+            {
+                if (*initial == delim[i])
+                {
+                    *initial  = '\0';
+                    return ++initial;
+                }
+                i++;
+            }
+            initial++;
+        }
+        return NULL;
+    }
 }
 
 int split_Namestnik(const char *string, char matrix[][N], const char symbol)
