92d8118aa9a9981cd014dbb5a72132b523c8c420
Sverdlov Sergey <701-ssa18u822@users.noreply.git.iu7.bmstu.ru>
2019-05-05 21:00:33 +0300

    Add split SergeySverdlov.h
    Former-commit-id: e1e6997399898567f19949cda860c342842fb371

--- STRGame/functions/SergeySverdlov.h
+++ STRGame/functions/SergeySverdlov.h
@@ -7,6 +7,20 @@ char *strtok_Sverdlov(char *string, const char *delim)
 
 int split_Sverdlov(const char *string, char matrix[][N], const char symbol)
 {
-    return 0;
+    int i = 0;
+    int j = 0;
+    while (*string != '\0')
+    {
+        if (*string == symbol)
+        {
+            matrix[i++][j] = '\0';
+            j = 0;
+        }
+        else
+            matrix[i][j++] = *string;
+        string ++;
+    }
+    matrix[i][j] = '\0';
+    i ++;
+    return i;
 }
-
