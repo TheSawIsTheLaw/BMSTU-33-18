65fcfa36338cd8154f16d82687de49f9d58cb8b8
kvo18u333 <krivozubovvo@student.bmstu.ru>
2019-05-05 13:04:19 +0300

    ADD: functions from KV for STRGame
    Former-commit-id: 621a9187a49198f6f274c6d159942129136d7451

--- STRGame/functions/VladKrivozubov.h
+++ STRGame/functions/VladKrivozubov.h
@@ -1,12 +1,108 @@
-// N - максимальная длина строки, она будет объявлена в мейне
+﻿// N - максимальная длина строки, она будет объявлена в мейне
 
 char *strtok_Krivozubov(char *string, const char *delim)
 {
-    return 0;
-}
+    static char *index;
+    register int i = 0, j = 0;
+    if (string == NULL)
+    {
+        if (*index == '\0')
+            return NULL;
+        else
+        {
+            string = index;
+            while (string[i])
+            {
+                j = 0;
+                while (delim[j])
+                {
+                    if (string[i] == delim[j])
+                        break;
+                    index = &string[i + 1];
+                    j = 0;
+                    while(*index)
+                    {
+                        j = 0;
+                        while (delim[j])
+                        {
+                            if (*index == delim[j])
+                            {
+                                *index = '\0';
+                                break;
+                            }
+                            j++;
+                        }
+                        if (*index == '\0')
+                        {
+                            index++;
+                            break;
+                        }
+                        else
+                            index++;
+                    }
+                    return &string[i];
+                    j++;
+                }
+                i++;
+            }   
+        }
+
+    }
+    else
+    {
+        i = 0; j = 0;
+        while (string[i] != '\0')
+        {
+            j = 0;
+            while (delim[j])
+            {
+                if (string[i] == delim[j])
+                    break;
+                index = &string[i + 1];
+                j = 0;
+                while(*index)
+                {
+                    j = 0;
+                    while (delim[j])
+                    {
+                        if (*index == delim[j])
+                        {
+                            *index = '\0';
+                            break;
+                        }
+                        j++;
+                    }
+                    if (*index == '\0')
+                    {
+                        index++;
+                        break;
+                    }
+                    else
+                        index++;
+                }
+                return &string[i];
+                j++;
+            }
+            i++;
+        }
+        return NULL;
+    }    
 
 int split_Krivozubov(const char *string, char matrix[][N], const char symbol)
 {
-    return 0;
+    register int i = 0, j = 0, current = 0;
+    while(string[current])
+    {
+        if (string[current] != symbol)
+            matrix[i][j++] = string[current];
+        else
+        {
+            matrix[i++][j] = '\0';
+            j = 0;
+        }
+        current++;
+    }
+    matrix[i][j] = '\0';
+    return ++i;
 }
 
