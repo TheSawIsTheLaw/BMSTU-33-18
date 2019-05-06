87357bd226bfc548abcd23ae15bfbd002aba5df5
Simonenko Emil <s_emil@inbox.ru>
2019-05-06 14:07:15 +0300

    Updated with functions from second branch
    Former-commit-id: 4a07ab57db9748e53bce47f201fc1f0cead25aeb

--- STRGame/functions/EmilSimonenko.h
+++ STRGame/functions/EmilSimonenko.h
@@ -1,13 +1,77 @@
-// N - максимальная длина строки, она будет объявлена в мейне
+// N - max str size
 
 char *strtok_Simonenko(char *string, const char *delim)
-{
-    return 0;
+    static char *past;
+    int i = 0, j = 0;
+
+    if (string == NULL)
+    {
+        while (*past)
+        {
+            while (delim[i])
+            {
+                if (*past == delim[i])
+                {
+                    *past = '\0';
+                    return ++past;
+                }
+
+                i++;
+            }
+
+            past++;
+        }
+
+        return NULL;  
+    }
+
+    else
+    {
+        past = string;
+        while (string[i])
+        {
+            while (delim[j])
+            {
+                if (string[i] == delim[j])
+                {
+                    string[i] = '\0';
+                    break;
+                }
+
+                j++;
+            }
+
+            i++;
+        }
+
+        return past;     //teper luchshe
+    }
 }
+
 
 int split_Simonenko(const char *string, char matrix[][N], const char symbol)
 {
-    // пасхалочка
+    int i = 0;
+    int c = 0;
+    int r = 0;
+
+    while (string[i])
+    {
+        if (symbol == string[i])
+        {
+            matrix[r][c] = "/0";
+            c = 0;
+            ++r;
+        }
+        else
+        {
+            matrix[r][c] = string[i];
+            ++c;
+        }
+        ++i;
+    }
+    matrix[r][c] = "/0";
+    return ++r;
+
     return 0;
 }
-
