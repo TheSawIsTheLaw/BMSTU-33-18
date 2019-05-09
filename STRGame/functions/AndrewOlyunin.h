94a8e862f0292e304a8cbc5e8a16469fbb68cc83
Olyunin Andrey <678-oad18u795@users.noreply.git.iu7.bmstu.ru>
2019-05-10 01:55:10 +0300

    updated split(wrong variable name)
    Former-commit-id: cec6cf6b144d5ec142e003e69754b44a2d50e5da

--- STRGame/functions/AndrewOlyunin.h
+++ STRGame/functions/AndrewOlyunin.h
@@ -3,12 +3,12 @@ int strtok(char *string, const char *delim)
 	return 0;	
 }
 
-int split(const char *string, char word_matrix[][N], const char splt_symbol)
+int split(const char *string, char word_matrix[][N], const char split_symbol)
 {
 	register int row = 0, col = 0, k = 0;
 	while(string[k])
 	{
-		if(string[k] == symbol)
+		if(string[k] == split_symbol)
 		{
 			word_matrix[row++][col] = '\0';
 			col = 0;
