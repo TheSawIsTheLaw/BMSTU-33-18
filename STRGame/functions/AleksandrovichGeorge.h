66e9e80f5f5d702b5b5968ecec8b4f797c23566a
Aleksandrovich George <679-agyu18u109@users.noreply.git.iu7.bmstu.ru>
2019-05-11 01:45:17 +0300

    Update
    Former-commit-id: b0cae0eff3398eaf1fa3b7ba833a3827b70d0f99

--- STRGame/functions/AleksandrovichGeorge.h
+++ STRGame/functions/AleksandrovichGeorge.h
@@ -1,9 +1,9 @@
-int strtok(char *string, const char *delim)
+int strtok_Aleksandrovich(char *string, const char *delim)
 {
 	return 0;
 }
 
-int split(const char *string, char w_matrix[][N], const char symbol)
+int split_Aleksandrovich(const char *string, char w_matrix[][N], const char symbol)
 {
 	register int i = 0, j = 0, k = 0;
 	while(string[k])
