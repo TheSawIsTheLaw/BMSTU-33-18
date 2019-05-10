de846be37b658faa17b6b294e42bea07e1bb151e
Namestnik Anastasiia <741-naa18u403@users.noreply.git.iu7.bmstu.ru>
2019-05-10 03:18:17 +0300

    Fix strtok()
    Former-commit-id: 1d3773d1e6fe3f6037bd998d8690bb52d56d6d61

--- STRGame/functions/AnastasiiaNamestnik.h
+++ STRGame/functions/AnastasiiaNamestnik.h
@@ -30,21 +30,23 @@ char *strtok_Namestnik(char *string, const char *delim)
     while (*next)
     {
         i = 0;
-        while (delim[i])
-        {
-            if (*next == delim[i])
-            {
-                *next++ = '\0';
-                flag = 1;
-                break;
-            }
+        while (*next != delim[i] && delim[i] != '\0')
             i++;
-        }
-        if (flag)
+        if (delim[i] == '\0')
+            ++next;
+        else
             break;
-        next++;
     }
-   
+    while (*next)
+    {
+        i = 0;
+        while (*next != delim[i] && delim[i] != '\0')
+            i++;
+        if (delim[i] == '\0')
+            break;
+        else
+            *next++ = '\0';
+    }
     return string;
 }
 int split_Namestnik(const char *string, char matrix[][N], const char symbol)
