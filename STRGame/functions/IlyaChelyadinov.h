cab5a940d68eaa28762a6958c10c89960bc26466
Chelyadinov Ilya <733-chid18u575@users.noreply.git.iu7.bmstu.ru>
2019-05-11 00:15:23 +0300

    LALKA ()())())
    Former-commit-id: 94950356238fccb5564f7d6be621e34d7ae31cb8

--- STRGame/functions/IlyaChelyadinov.h
+++ STRGame/functions/IlyaChelyadinov.h
@@ -6,7 +6,7 @@ char *strtok_Chelyadinov(char *string, const char *delim)
 }
 
 int split_Chelyadinov(const char *string, char matrix[][N], const char symbol)
-{/*
+{
     register int i = 0, j = 0, k = 0;
     while (string[k])
     {
@@ -19,8 +19,7 @@ int split_Chelyadinov(const char *string, char matrix[][N], const char symbol)
             matrix[i][++j] =  string[k];
         ++k;
     }
-    matrix[i][j] = "\0";
-    return ++i;*/
-    return 0;
+    matrix[i][j] = '\0';
+    return ++i;
 }
 
