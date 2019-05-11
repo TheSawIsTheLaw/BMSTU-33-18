262f6dadd4a81624cb28cb7ba0f3e161690aaa60
Pavel Perestoronin <p_perestornin@mail.ru>
2019-05-11 10:21:31 +0300

    trying to boost split
    Former-commit-id: c1ab8efc0b2828178192e369f4f2019e30ee49dc

--- STRGame/functions/PavelPerestoronin.h
+++ STRGame/functions/PavelPerestoronin.h
@@ -51,15 +51,15 @@ char *strtok_Perestoronin(char *string, const char *delim)
 int split_Perestoronin(const char *string, char matrix[][N], const char symbol)
 {
     register int i = 0, j = 0, k = 0;
-    while (string[k] != '\0')
+    while (string[k])
     {
-        if (string[k] == symbol)
+        if (string[k] != symbol)
+            matrix[i][j++] = string[k];
+        else
         {
             matrix[i++][j] = '\0';
             j = 0;
         }
-        else
-            matrix[i][j++] = string[k];
         k++;
     }
     matrix[i][j] = '\0';
