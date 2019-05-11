7db11c432ffbda38217ad8f6142a7eac1f4ea05a
Chelyadinov Ilya <733-chid18u575@users.noreply.git.iu7.bmstu.ru>
2019-05-11 11:13:19 +0300

    FINALOCHKA
    Former-commit-id: 30df81adde58c703764a6deacb79bf7328b24262

--- STRGame/functions/IlyaChelyadinov.h
+++ STRGame/functions/IlyaChelyadinov.h
@@ -1,8 +1,67 @@
 // N - максимальная длина строки, она будет объявлена в мейне
 
-char *strtok_Chelyadinov(char *string, const char *delim)
+int my_strspn(const char *str1,const char *str2){ 
+    int i,k,counter=0; 
+    for(i=0;str1[i]!='\0';i++){ 
+        if(counter != i) 
+            break; 
+        for(k=0;str1[k]!='\0';k++){ 
+            if(str1[i]==str2[k]) 
+            counter++; 
+        } 
+    } 
+    return counter; 
+}
+unsigned short mystrlen(char *str) {
+    for (int i = 0;; i++) 
+        if (str[i] == '\0') 
+            return i;
+}
+
+
+char *strtok_Chelyadinov(char *str, const char *delim)
 {
-    return 0;
+    static char *next;
+    static int size;
+    int i;
+    if (str != NULL)
+    {
+        next = str;
+        size = strlen(str);
+    }
+    else if (size > 0)
+    {
+        ++next;
+        size--;
+        str = next;
+    }
+    else
+    {
+        str = NULL
+    }
+    while (*next)
+    {
+        i = my_strspn(next, delim);
+        while (i>1)
+        {
+            *next = '\0';
+            ++next;
+            size--;
+            i--;
+        }
+        if (i == 1)
+        {
+            *next = '\0';
+            if (size > 1)
+            {
+                next--;
+                ++size;
+            }
+        }
+        ++next;
+        size--;
+    }
+    return str;
 }
 
 int split_Chelyadinov(const char *string, char matrix[][N], const char symbol)
