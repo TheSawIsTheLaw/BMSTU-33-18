f5cd168eda1d20894564f1e00e1bc9f6c8c1f2ff
U-VLAB\user-lab01 <user-lab01@LAB-01-15.vlab.bmstu.ru>
2019-05-11 22:32:01 +1400

    added fixes
    Former-commit-id: 985a7c23657b8bc2179ba9397403d37cfdbea9ef

--- STRGame/functions/EmilSimonenko.h
+++ STRGame/functions/EmilSimonenko.h
@@ -2,50 +2,41 @@
 #define BSZ '\0'
 char *strtok_Simonenko(char *string, const char *delim)
 {
-    static char *past;
-    static char *last;
+    static char *check;
+    int i = 0, j = 0;
 
-    if (string == NULL)
+    if (string)
+        check = string;
+	
+    if ( (*check == BSZ) || (check == NULL) )
+        return NULL;
+	
+    while(check)
     {
-    
-		char *now = past;
-        while (*now != BSZ && now != last)
-			now++;							//now we reached lexema
-		while (*now == BSZ && now != last)
-			now++;
-		if (now == last)
-			return NULL;
-		else
-		past = now;
-		return now;
-	}
-    else
-    {
-        last = string;
-		char *before = string;
-		int i = 0;
-        while (*last != 0)
-			last++;
-		while (delim[i++])    //OP++ returns its prevous znachenie and then increases operand
-		{
-			while (*before == delim[i] && before != last)
-				before++;
-			if (before == last)
-				return NULL;
-		}
-		i = 0;
-		char *rightnow = before;
-		while (rightnow++ != last)
-		{
-			while (delim[i++])
-				if (*rightnow == delim[i-1])
-					*rightnow = BSZ;
-			i = 0;
-		}      
-		past = before;
-        return before;     //teper luchshe
+        for (i = 0; delim[i]; ++i)
+            if (*check == delim[i])
+            {
+                ++check;
+                break;
+            }
+        if (!delim[i])
+            break;
     }
-	return NULL;
+	
+    i = 0;
+    while (check[i++])
+        for (j = 0; delim[j]; ++j)
+            if (check[i] == delim[j])
+            {
+                if (check[i] == BSZ)
+                {
+                    check += i;
+                    return check - i;
+                }
+                check[i] = BSZ;
+                check += i + 1;
+                return check - i - 1;
+            }
 }
 
 
@@ -55,14 +46,17 @@ int split_Simonenko(const char *string, char matrix[][N], const char symbol)
     int c = 0;   //collumn
     int r = 0;   //row
 
-    while (string[i++])
-        if (symbol == string[i-1])
+    while (string[i])
+	{
+        if (symbol == string[i])
         {
             matrix[r++][c] = BSZ ;
             c = 0;
         }
         else
             matrix[r][c++] = string[i];
+		i++;
+	}
     matrix[r][c] = BSZ;
     return ++r;
 }
