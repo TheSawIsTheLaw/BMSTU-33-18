de30f8f241a0459248291e0560e1f4ca7bac5a9f
Kononenko Sergey <sergkononenko@mail.ru>
2019-05-16 01:03:53 +0300

    Update rungame script with creation of gamelog files
    Former-commit-id: 6b869686f91140fb6f61cc0726e9f7cef3fff882

--- XOgame/rungame.sh
+++ XOgame/rungame.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+touch 3x3gamelog.txt && touch 5x5gamelog.txt &&
 gcc -o 3x3division.exe 3x3division.c &&
 ./3x3division.exe > 3x3gamelog.txt &&
 gcc -o 5x5division.exe 5x5division.c &&
