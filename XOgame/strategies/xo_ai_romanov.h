9ac714687862528b07f937c52e1684015d9674c5
Romanov Alexey <gta.sanandreassamp@yandex.ru>
2019-05-16 01:54:39 +0300

    Upload super-imba-flex startegy for X0 game
    Former-commit-id: d644ed147150d516bdfdfa7f9407490e0d4b2e09

--- XOgame/strategies/xo_ai_romanov.h
+++ XOgame/strategies/xo_ai_romanov.h
@@ -41,6 +41,132 @@ void make_shot_romanov(char symb, char BF[][DIME])
 
     else
     {
+        char second;
+        if (symb == 'X')
+        {
+            second = 'O';
+        }
+        else
+        {
+            second = 'X';
+        }
+
+        for (int i = 0; i < DIME; i++)
+        {
+            int count = 0;
+            for (int j = 0; j < DIME; j++)
+            {
+                if (BF[i][j] == second)
+                {
+                    ++count;
+                }
+            }
+            if (DIME - 1 == count)
+            {
+                int k = 0;
+                int checker = 0;
+                while (BF[i][k] != ' ' && k < DIME)
+                {
+                    ++k;
+                    checker = 1;
+                }
+                if (checker || BF[i][k] == ' ')
+                {
+                    if (k <= (DIME - 1))
+                    {
+                        BF[i][k] = symb;
+                        return;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < DIME; i++)
+        {
+            int count = 0;
+            for (int j = 0; j < DIME; j++)
+            {
+                if (BF[j][i] == second)
+                {
+                    ++count;
+                }
+            }
+            if (DIME - 1 == count)
+            {
+                int k = 0;
+                int checker = 0;
+                while (BF[k][i] != ' ' && k < DIME)
+                {
+                    ++k;
+                    checker = 1;
+                }
+                if (checker || BF[k][i] == ' ')
+                {
+                    if (k <= (DIME - 1))
+                    {
+                        BF[k][i] = symb;
+                        return;
+                    }
+                }
+            }
+        }
+        
+        int count = 0;
+        for (int i = 0; i < DIME; i++)
+        {
+            if (BF[i][i] == second)
+            {
+                ++count;
+            }
+
+            if (DIME - 1 == count)
+            {
+                int k = 0;
+                int checker = 0;
+                while (BF[k][k] != ' ' && k < DIME)
+                {
+                    ++k;
+                    checker = 1; 
+                }
+                if (checker || BF[k][k] == ' ')
+                {
+                    if (k <= (DIME - 1))
+                    {
+                        BF[k][k] = symb;
+                        return;
+                    }
+                }
+            }
+        }
+        
+        count = 0;
+        for (int i = 1; i <= DIME; i++)
+        {
+            if (BF[i - 1][DIME - i] == second)
+            {
+                ++count;
+            }
+
+            if (DIME - 1 == count)
+            {
+                int k = 1;
+                int checker = 0;
+                while (BF[k - 1][DIME - k] != ' ' && k <= DIME)
+                {
+                    ++k;
+                    checker = 1;
+                }
+                if (checker || BF[k - 1][DIME - k] == ' ')
+                {
+                    if (k <= DIME)
+                    {
+                        BF[k - 1][DIME - k] = symb;
+                        return;
+                    }
+                }
+            }
+        }
+
         int i_random = DIME - 1;
         int j_random = DIME - 1;
 
@@ -49,7 +175,6 @@ void make_shot_romanov(char symb, char BF[][DIME])
             i_random = rand() % DIME;
             j_random = rand() % DIME;
         }
-
         BF[i_random][j_random] = symb;
     }
 }
