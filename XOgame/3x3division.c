905e853ec32e4561704f0a0cd9f2f96a9cd7a09c
Kononenko Sergey <sergkononenko@mail.ru>
2019-05-17 01:23:19 +0300

    Add Yakuba to players list
    Former-commit-id: 3ebebd7f89a702f466e580727d1c653a586ce5d0

--- XOgame/3x3division.c
+++ XOgame/3x3division.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 #define DIME 3
-#define STUDENTS 5
+#define STUDENTS 6
 
 #define GAME_OVER 0
 #define WIN_PLAYER_ONE -1
@@ -113,7 +113,8 @@ int main()
             "Krivozubov Vladislav",
             "Romanov Alexey",
             "Prokhorova Lyubov",
-            "Chernenko Vladislav"
+            "Chernenko Vladislav",
+            "Dmitriy Yakuba"
     };
 
     xo_strategy xo_strategies[] = {
@@ -121,7 +122,8 @@ int main()
             make_shot_krivozubov,
             make_shot_romanov,
             make_shot_prokhorova,
-            make_shot_chernenko
+            make_shot_chernenko,
+            make_shot_yakuba
     };
 
     for (int i = 0; i < STUDENTS - 1; ++i)
