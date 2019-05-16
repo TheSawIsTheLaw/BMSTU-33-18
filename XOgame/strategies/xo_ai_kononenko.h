91c264cc698d3bc3e6a443a1b29361a630f827eb
Kononenko Sergey <sergkononenko@mail.ru>
2019-05-17 00:40:36 +0300

    Update xo_ai_kononenko.h with WIP label
    Former-commit-id: d607e560f027e0bba2d70a293405cb88daf8de6a

--- XOgame/strategies/xo_ai_kononenko.h
+++ XOgame/strategies/xo_ai_kononenko.h
@@ -1,12 +1,4 @@
-#include <stdlib.h>
-
-void make_shot_kononenko(const char symb, char BF[][DIME])
+void make_shot_kononenko(char symb, char BF[][DIME])
 {
-    int random_coord_x = rand() % DIME;
-    int random_coord_y = rand() % DIME;
-
-    while (BF[random_coord_x][random_coord_y] != ' ')
-        random_coord_x = rand() % DIME;
-
-    BF[random_coord_x][random_coord_y] = symb;
+    // WIP
 }
