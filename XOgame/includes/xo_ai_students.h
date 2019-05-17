298bec2ec41d4a57e0d479715a6038ec0da94ba7
Kononenko Sergey <sergkononenko@mail.ru>
2019-05-17 22:01:05 +0300

    Add header for students lists
    Former-commit-id: 28f93782d5eb6dcf2cd2ff4d6191c7c3c1d6fd66

---
+++ XOgame/includes/xo_ai_students.h
@@ -0,0 +1,23 @@
+const char *students[] = {
+        "Dmitriy Kovalev",
+        "Krivozubov Vladislav",
+        "Romanov Alexey",
+        "Prokhorova Lyubov",
+        "Chernenko Vladislav",
+        "Dmitriy Yakuba",
+        "Sukocheva Alis",
+        "Perestoronin Pavel",
+        "Kononenko Sergey"
+};
+
+xo_strategy xo_strategies[] = {
+        make_shot_kovalev,
+        make_shot_krivozubov,
+        make_shot_romanov,
+        make_shot_prokhorova,
+        make_shot_chernenko,
+        make_shot_yakuba,
+        make_shot_sukocheva,
+        make_shot_perestoronin,
+        make_shot_kononenko
+};
