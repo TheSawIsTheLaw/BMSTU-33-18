d2d6c3e1459429b5b20cc3108371ee4c40f122c1
Kononenko Sergey <sergkononenko@mail.ru>
2019-05-17 21:54:42 +0300

    Update xo_ai_kononenko.h with not-super-fleksss-imba strategy
    Former-commit-id: f4e5c554c522eeb8ef0383bd1223ea8c70585678

--- XOgame/strategies/xo_ai_kononenko.h
+++ XOgame/strategies/xo_ai_kononenko.h
@@ -1,4 +1,60 @@
+#define GOOD 1
+#define BAD 0
+
+int check_row_sk(char symb, char BF[][DIME])
+{
+    for (int i = 0; i < DIME; i++)
+        for (int j = 0; j < DIME; j++)
+            if (BF[i][j] == ' ')
+            {
+                BF[i][j] = symb;
+                return GOOD;
+            }
+    return BAD;
+}
+
+int check_col_sk(char symb, char BF[][DIME])
+{
+    for (int j = 0; j < DIME; j++)
+        for (int i = 0; i < DIME; i++)
+            if (BF[i][j] == ' ')
+            {
+                BF[i][j] = symb;
+                return GOOD;
+            }
+    return BAD;
+}
+
+int check_side_diag_sk(char symb, char BF[][DIME])
+{
+    for (int i = 1; i <= DIME; i++)
+        if (BF[i - 1][DIME - i] == ' ')
+        {
+            BF[i - 1][DIME - i] = symb;
+            return GOOD;
+        }
+    return BAD;
+}
+
+int check_main_diag_sk(char symb, char BF[][DIME])
+{
+    for (int i = 0; i < DIME; i++)
+        if (BF[i][i] == ' ')
+        {
+            BF[i][i] = symb;
+            return GOOD;
+        }
+    return BAD;
+}
+
 void make_shot_kononenko(char symb, char BF[][DIME])
 {
-    // WIP
+    if (check_main_diag_sk(symb, BF))
+        return;
+    if (check_side_diag_sk(symb, BF))
+        return;
+    if (check_col_sk(symb, BF))
+        return;
+    if (check_row_sk(symb, BF))
+        return;
 }
