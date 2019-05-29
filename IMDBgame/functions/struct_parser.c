6b805abf55d23707ad62c09d4bd1200d939aeaed
kvo18u333 <krivozubovvo@student.bmstu.ru>
2019-05-29 23:01:18 +0300

    ADD: parser for struct(not filling id and name fields)
    Former-commit-id: 18e506ee25c0637638ec0a684e77485397f0d645

---
+++ IMDBgame/functions/struct_parser.c
@@ -0,0 +1,58 @@
+#include "node.h"
+
+#define MAX_NAME_SIZE 32
+
+// f_id_name - имя файла с id и именем актера
+// f_neighbours - имя файла с набором соседей актера
+// array - массив структур 
+
+typedef const char c_c;
+typedef unsigned int u_i;
+
+void cut_name(FILE* f_p, char *name)
+{
+    u_i length = 0u;
+    char symb = fgetc(f_p);
+    while (symb == ' ')
+        symb = fgetc(f_p);
+    if (symb == '\n')
+    {
+        name[0] = '\0';
+    }
+    else
+    {
+        ungetc(symb, f_p);
+        while (symb != '\n')
+        {
+            if (length == MAX_NAME_SIZE - 1)
+                break;
+            name[length] = symb;
+            length++;
+            symb = fgetc(f_p);
+        }
+        name[length] = '\0';
+    }
+    while (symb != '\n')
+        symb = fgetc(f_p);
+}
+
+void fill_struct(c_c *const f_id_name, c_C *const f_neighbours, node *const array)
+{
+
+    FILE *fp_1 = fopen(f_id_name, "r");
+    FILE *fp_2 = fopen(f_neighbours, "r");
+
+    if (fp_1 && fp_2)
+    {
+        u_i i = 0u; // Номер записи
+        char ending = ;
+        while (!feof(fp_2))
+        {
+            fscanf(fp_1, "%u", &(array[i].id));
+            cut_name(fp_1, array[i].name);
+            array[i] = structure;
+            i++;
+            ending = fgetc(fp_2)
+        }
+    }
+}
