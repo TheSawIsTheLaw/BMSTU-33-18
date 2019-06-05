4c958c9bc9b1e23f1be882d6e9cce87f4870b629
Pavel Perestoronin <p_perestoronin@mail.ru>
2019-06-05 12:32:43 +0300

    added last vers of sort
    Former-commit-id: 7e54eaeb50dc2e30903fd1caf632ab6f36440fbf

--- IMDBgame/after_sem/snake_solution.c
+++ IMDBgame/after_sem/snake_solution.c
@@ -4,15 +4,46 @@
 #define YES 1
 #define PAIR_LEN 2
 
-int is_less(int *elem_1, int *elem_2)
+/*
+typedef struct
 {
-    if (elem_1[0] < elem_2[0] || (elem_1[0] == elem_2[0] && elem_1[1] < elem_2[1]))
+    unsigned int vertex_id : 24;
+    unsigned int bacon : 8;
+} Tree;
+*/
+
+
+void add_conn(unsigned int *const conn_arr, const unsigned int new_conn_id)
+{
+    conn_arr[new_conn_id / (sizeof(unsigned int) * 8)] |= 1 << (new_conn_id % (sizeof(unsigned int) * 8));
+}
+
+
+unsigned int get_conn(unsigned int *const conn_arr, const unsigned int id)
+{
+    return conn_arr[id / (sizeof(unsigned int) * 8)] & (1 << (id % (sizeof(unsigned int) * 8)));
+}
+
+
+int is_less(const int *const elem_1, const int *const elem_2, const int index)
+{
+    if (elem_1[index] < elem_2[index] || (elem_1[index] == elem_2[index]
+        && elem_1[(index + 1) % 2] < elem_2[(index + 1) % 2]))
         return YES;
     return NO;
 }
 
 
-void swap_pair(int *elem_1, int *elem_2)
+int is_less_equal(const int *const elem_1, const int *const elem_2, const int index)
+{
+    if (elem_1[index] < elem_2[index] || (elem_1[index] == elem_2[index] &&
+        elem_1[(index + 1) % 2] <= elem_2[(index + 1) % 2]))
+        return YES;
+    return NO;
+}
+
+
+void swap_pair(int *const elem_1, int *const elem_2)
 {
     int temp;
     for (int i = 0; i < PAIR_LEN; i++)
@@ -24,7 +55,7 @@ void swap_pair(int *elem_1, int *elem_2)
 }
 
 
-int get_pos_by_id(int main_matr[][PAIR_LEN], int id, int len)
+int get_pos_by_id(const int main_matr[][PAIR_LEN], const int id, const int len, const int index)
 {
     int left = 0;
     int right = len - 1;
@@ -33,9 +64,9 @@ int get_pos_by_id(int main_matr[][PAIR_LEN], int id, int len)
     while (left <= right)
     {
         middle = (left + right) / 2;
-        if (main_matr[middle][0] < id)
+        if (main_matr[middle][index] < id)
             left = middle + 1;
-        else if (main_matr[middle][0] > id)
+        else if (main_matr[middle][index] > id)
             right = middle - 1;
         else
             goto found;
@@ -43,12 +74,12 @@ int get_pos_by_id(int main_matr[][PAIR_LEN], int id, int len)
     return -1;
 
     found:
-    while (main_matr[middle--][0] == id && middle >= 0);
-    return (middle == -1) ? middle + 1 : middle + 2;
+        while (main_matr[middle--][index] == id && middle >= 0);
+        return (middle == -1) ? middle + 1 : middle + 2;
 }
 
 
-void my_sort(int main_matr[][PAIR_LEN], int begin, int end) // qsort(main_matr, 0, size - 1)
+void my_sort(int main_matr[][PAIR_LEN], const int begin, const int end, const int index) // qsort(main_matr, 0, size - 1)
 {
     if (begin >= end)
         return;
@@ -59,9 +90,9 @@ void my_sort(int main_matr[][PAIR_LEN], int begin, int end) // qsort(main_matr, 
 
     while (left <= right)
     {
-        while (is_less(main_matr[left], middle))
+        while (is_less(main_matr[left], middle, index))
             left++;
-        while (is_less(middle, main_matr[right]))
+        while (is_less_equal(middle, main_matr[right], index))
             right--;
         if (left <= right)
             swap_pair(main_matr[left++], main_matr[right--]);
@@ -70,63 +101,75 @@ void my_sort(int main_matr[][PAIR_LEN], int begin, int end) // qsort(main_matr, 
     my_sort(main_matr, left, end);
 }
 
-/*
-// snake_solution(films_matr, actors_matr, len, from, to, 0, 20, route)
-int snake_solution(int films_matr[][PAIR_LEN], int actors_matr[][PAIR_LEN],
-    int len, int from, int to, int iter, int max_iter, int *route)
+
+void make_tree(unsigned int *main_tree, actors_pairs[][PAIR_LEN],
+    const int size_of_actors, const int from, const int to) // id of main vertex of tree (Bacon's)
 {
-    if (iter >= max_iter)
-        return -1;
-    else if (from == to)
-        return 0;
+    unsigned int queue[size_of_actors]; // очередь для вершин для поиска в ширину
+    unsigned int visited[size_of_actors / 32 + 1] = { 0 }; // список посещенных вершин
+    unsigned int cur_pos = 0, max_pos = 0; // начало, конец очереди
+    unsigned int id = from;
+    unsigned int cur_id;
 
-    int pos, min_pos;
-    int min_dist = max_iter + 1, dist;
+    main_tree[id] = 0;
+    add_conn(visited, id);
+    queue[max_pos++] = id;
 
-    if (!(iter % 2))
+    while(cur_pos < max_pos) // пока очередь не пуста
     {
-        pos = get_pos_by_id(actors_matr, from, len);
-        while (actors_matr[pos][0] == from)
-            if ((dist = snake_solution(films_matr, actors_matr, len, actors_matr[pos++][1], to, iter + 1, max_iter, route + 1)) != -1)
-            {
-                min_dist = (min_dist > dist) ? dist : min_dist;
-                min_pos = pos - 1;
-            }
-        if (min_dist < max_iter + 1)
+        id = queue[cur_pos++]; // берем следующего по очереди актера
+        index = get_pos_by_id(actors_pairs, id, len, 0);
+        while (actors_pairs[index][0] == id) // найдем все необработанные связи для этого актера...
         {
-            *route = min_pos;
-            return min_dist;
-        }
-        else
-            return -1;
-    }
-
-    else
-    {
-        pos = get_pos_by_id(films_matr, from, len);
-        while (films_matr[pos][0] == from)
-            if ((dist = snake_solution(films_matr, actors_matr, len, films_matr[pos++][1], to, iter + 1, max_iter, route + 1)) != -1)
+            cur_id = actors_pairs[index][1];
+            if (!get_conn(visited, cur_id)) // если найдена связь и она еще не была обработана
             {
-                min_pos = (min_dist > dist) ? pos - 1 : min_pos;
-                min_dist = (min_dist > dist) ? dist : min_dist;
+                add_conn(visited, cur_id); // добавить связь в обработанные
+                queue[max_pos++] = cur_id; // добавить данного актера в очередь
+                main_tree[cur_id] = id; // записать данные для данного актера
             }
-        if (min_dist < max_iter + 1)
-        {
-            *route = min_pos;
-            return min_dist;
-        }
-        else
-            return -1;
     }
 }
-*/
+
+
+void get_route(const unsigned int *const main_tree, const int index, unsigned int *route)
+{
+    int i = 0;
+    route[i++] = index;
+    while(main_tree[index] != 0)
+    {
+        route[i++] = main_tree[index];
+        index = main_tree[index];
+    }
+}
+
+
+void set_test(int test_arr[][PAIR_LEN], const int num_1, const int *nums,
+    const int len_nums, const int *pos)
+{
+    int i;
+    for (i = 0; i < len_nums; i++)
+    {
+        test_arr[*pos + i][0] = num_1;
+        test_arr[*pos + i][1] = num_2;
+    }
+    *pos += i;
+}
+
+
+int fill_test(int test_arr[][PAIR_LEN])
+{
+    int pos = 0;
+    set_test(test_arr, 1, 2, &pos);
+}
+
 
 int main()
 {
     setbuf(stdout, NULL);
 
-    int test_arr[][PAIR_LEN] = { { 234, 234 }, { 3242, 324 }, { 3142, 724 }, { 123, 121 }, { 234, 657 },
-    { 34, 3412 }, { 34, 34 }, { 34, 32 }, { 3142, 2 }, { 324, 100 } };
+    int test_arr[100][PAIR_LEN];
+    int size = fill_test(test_arr);
 
     my_sort(test_arr, 0, 9);
     for (int i = 0; i < 10; i++)
@@ -137,3 +180,56 @@ int main()
     printf("\n%d", get_pos_by_id(test_arr, 3142, 10));
     return 0;
 }
+
+
+
+/*
+// snake_solution(films_matr, actors_matr, len, from, to, 0, 20, route)
+int snake_solution(int films_matr[][PAIR_LEN], int actors_matr[][PAIR_LEN],
+int len, int from, int to, int iter, int max_iter, int *route)
+{
+if (iter >= max_iter)
+return -1;
+else if (from == to)
+return 0;
+
+int pos, min_pos;
+int min_dist = max_iter + 1, dist;
+
+if (!(iter % 2))
+{
+pos = get_pos_by_id(actors_matr, from, len);
+while (actors_matr[pos][0] == from)
+if ((dist = snake_solution(films_matr, actors_matr, len, actors_matr[pos++][1], to, iter + 1, max_iter, route + 1)) != -1)
+{
+min_dist = (min_dist > dist) ? dist : min_dist;
+min_pos = pos - 1;
+}
+if (min_dist < max_iter + 1)
+{
+*route = min_pos;
+return min_dist;
+}
+else
+return -1;
+}
+
+else
+{
+pos = get_pos_by_id(films_matr, from, len);
+while (films_matr[pos][0] == from)
+if ((dist = snake_solution(films_matr, actors_matr, len, films_matr[pos++][1], to, iter + 1, max_iter, route + 1)) != -1)
+{
+min_pos = (min_dist > dist) ? pos - 1 : min_pos;
+min_dist = (min_dist > dist) ? dist : min_dist;
+}
+if (min_dist < max_iter + 1)
+{
+*route = min_pos;
+return min_dist;
+}
+else
+return -1;
+}
+}
+*/
