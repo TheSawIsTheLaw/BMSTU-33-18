08c29c5467b9a16a540f4f419dff851e123ab2f4
Paw.toporkov <pawtoporkov@MacBook-Pro-Pawtoporkov.local>
2019-06-06 23:20:48 +0300

    fix
    Former-commit-id: 25c885e32e0e7a48048171fb104c874723058acc
    Former-commit-id: 84f3fb55b46ca9ee2b077855740a278948692cd9

--- IMDBgame/functions/last_version/create_arrs.h
+++ IMDBgame/functions/last_version/create_arrs.h
@@ -1,4 +1,93 @@
+
 #define SIZE 3694389
+
+void add_conn(int *const conn_arr, const int new_conn_id)
+{
+    conn_arr[new_conn_id / (sizeof(unsigned int) * 8)] |= 1 << (new_conn_id % (sizeof(unsigned int) * 8));
+}
+
+
+unsigned int get_conn(int *const conn_arr, const int id)
+{
+    return conn_arr[id / (sizeof(unsigned int) * 8)] & (1 << (id % (sizeof(unsigned int) * 8)));
+}
+
+
+int is_less(const int *const elem_1, const int *const elem_2, const int index)
+{
+    if (elem_1[index] < elem_2[index] || (elem_1[index] == elem_2[index]
+                                          && elem_1[(index + 1) % 2] < elem_2[(index + 1) % 2]))
+    return YES;
+    return NO;
+}
+
+
+int is_less_equal(const int *const elem_1, const int *const elem_2, const int index)
+{
+    if (elem_1[index] < elem_2[index] || (elem_1[index] == elem_2[index] &&
+                                          elem_1[(index + 1) % 2] <= elem_2[(index + 1) % 2]))
+    return YES;
+    return NO;
+}
+
+
+void swap_pair(int *const elem_1, int *const elem_2)
+{
+    int temp;
+    for (int i = 0; i < PAIR_LEN; i++)
+    {
+        temp = elem_1[i];
+        elem_1[i] = elem_2[i];
+        elem_2[i] = temp;
+    }
+}
+
+
+int get_pos_by_id(int main_matr[][PAIR_LEN], const int id, const int len, const int index)
+{
+    int left = 0;
+    int right = len - 1;
+    int middle;
+    
+    while (left <= right)
+    {
+        middle = (left + right) / 2;
+        if (main_matr[middle][index] < id)
+        left = middle + 1;
+        else if (main_matr[middle][index] > id)
+        right = middle - 1;
+        else
+        goto found;
+    }
+    return -1;
+    
+found:
+    while (main_matr[middle--][index] == id && middle >= 0);
+    return (middle == -1) ? middle + 1 : middle + 2;
+}
+
+
+void my_sort(int main_matr[][PAIR_LEN], const int begin, const int end, const int index) // qsort(main_matr, 0, size - 1)
+{
+    if (begin >= end)
+    return;
+    
+    int middle_pos = (end + begin) / 2;
+    int middle[2] = { main_matr[middle_pos][0], main_matr[middle_pos][1] };
+    int left = begin, right = end;
+    
+    while (left <= right)
+    {
+        while (is_less(main_matr[left], middle, index))
+        left++;
+        while (is_less(middle, main_matr[right], index))
+        right--;
+        if (left <= right)
+        swap_pair(main_matr[left++], main_matr[right--]);
+    }
+    my_sort(main_matr, begin, right, index);
+    my_sort(main_matr, left, end, index);
+}
 
 void create_mas(int arr[][2], int *size, char *filename)
 {
@@ -29,11 +118,11 @@ void copy_mas(int source[][2], int destination[][2])
 // Сортировка по фильмам
 void sort_by_films(int arr[][2])
 {
-    //my_sort(arr, 0, SIZE - 1, 0);
+    my_sort(arr, 0, SIZE - 1, 0);
 }
 
 // Сортировка по актерам
 void sort_by_actors(int arr[][2])
 {
-    //my_sort(arr, 0, SIZE - 1, 1);
+    my_sort(arr, 0, SIZE - 1, 1);
 }
