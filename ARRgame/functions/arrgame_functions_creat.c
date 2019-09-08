ec5288c7c3528e3375e302af4696e5942645797f
ptrk <paw.toporkov@gmail.com>
2019-09-09 01:10:36 +0300

    fix
    Former-commit-id: e9e99c44d39a6a23e42086099451064a2092fd9d

--- ARRgame/functions/arrgame_functions_creat.c
+++ ARRgame/functions/arrgame_functions_creat.c
@@ -10,15 +10,45 @@
 
 int* creat(const int sizeArray)
 {
-    if (sizeArray < 1)
-        return NO_CORRECT_SIZE_ARRAY;
-    
+    //if (sizeArray < 1)
+        //return NO_CORRECT_SIZE_ARRAY;
+       
     int* pointUserStart;
-    
+    int allocatSize = 0;
+
+
     pointUserStart = 
-        (int*)malloc((sizeArray + COUNT_NEED_ELEMENT) * sizeof(int));
+        (int*)calloc((sizeArray + COUNT_NEED_ELEMENT),  sizeof(int));
+    if (pointUserStart)
+    {
+        int* allocatStart = realloc(pointUserStart,
+                (sizeArray * 2 + COUNT_NEED_ELEMENT));
+        if (allocatStart)
+        {
+            pointUserStart = allocatStart;
+            allocatStart = NULL;
+            allocatSize = sizeArray * 2;
+        }
+        else
+            allocatSize = sizeArray;
+    }
+    else
+    {
+        puts("Не удалось выделить память.");
+    }
     
-    pointUserStart += 3;
+    //array[0] - длинна массива, запрошенного пользователем
+    pointUserStart = sizeArray;
+    
+    //array[1] - количество аллоцированнх элементов(не считая первых 3х)
+    pointpointUserStart++;
+    pointUserStart = allocatSize;
+    
+    //array[2] - количество занятых ячеек
+    pointpointUserStart++;
+    pointUserStart = 0;
+
+    pointpointUserStart++;
 
     return pointUserStart;
 }
