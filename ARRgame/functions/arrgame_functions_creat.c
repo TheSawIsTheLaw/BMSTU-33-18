6ba2fa84660c306037f61afb2bfa755522c9e3c0
ptrk <paw.toporkov@gmail.com>
2019-09-09 00:45:39 +0300

    fix bags
    Former-commit-id: f6625fb6eee1251f03b293f73e5fa6be105803f6

--- ARRgame/functions/arrgame_functions_creat.c
+++ ARRgame/functions/arrgame_functions_creat.c
@@ -6,26 +6,57 @@
 #define COUNT_NEED_ELEMENT 3
 
 #define NO_CORRECT_SIZE_ARRAY 101
-
+#define MEMORY_ERROR 102
 
 int* creat(const int sizeArray)
 {
-    if (sizeArray < 1)
-        return NO_CORRECT_SIZE_ARRAY;
+    //if (sizeArray < 1)
+        //return NO_CORRECT_SIZE_ARRAY;
     
-    int* pointUserStart;
-    
+    int realSize = 0;
+    int* pointUserStart;   
     pointUserStart = 
-        (int*)malloc((sizeArray + COUNT_NEED_ELEMENT) * sizeof(int));
+        (int*)calloc((sizeArray + COUNT_NEED_ELEMENT), sizeof(int));
     
-    pointUserStart += 3;
-
+    if (pointUserStart)
+    {
+        int *newPointSize = realloc(pointUserStart, 
+                (sizeArray * 2 + COUNT_NEED_ELEMENT));
+        if (newPointSize)
+        {
+            realSize = sizeArray * 2; 
+            pointUserStart = newPointSize;
+            newPointSize = NULL;
+        }
+        else
+            realSize = sizeArray;
+    }
+    else
+    {
+        puts("Не удалось выделить память!!!");
+        //return MEMORY_ERROR;
+    }
+    
+    //array[0]
+    // размер массива
+    *pointUserStart = sizeArray; 
+    
+    //array[1]
+    // количество аллоцированных элементов (не учитывая первые три)
+    pointUserStart += 1;
+    *pointUserStart = realSize;
+    
+    //array[2]
+    // фактическая занятость массива
+    pointUserStart += 1;
+    *pointUserStart = 0;
     return pointUserStart;
 }
 
 /*
 int main()
 {
+    int* p = creat(10);
     return 0;
-}
-*/
+}*/
+
