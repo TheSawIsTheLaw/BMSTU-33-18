81bc071120a5546b4070ea778db31cc9f536bbb6
Romanov Alexey <gta.sanandreassamp@yandex.ru>
2019-09-12 19:46:45 +0300

    Reduce now works with threads
    Former-commit-id: 1494523930f893f229848f5f2e8a3c16d2bbb66a
    Former-commit-id: 84047dcb97997b5c49197abb9a929fc1ff814735

--- ARRgame/functions/arrgame_functions_reduce.c
+++ ARRgame/functions/arrgame_functions_reduce.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <omp.h>
 #include "../headers/arrgame_headers_reduce.h"
 
 #define  SIZE_ADRS -3
@@ -7,17 +8,22 @@ typedef struct
 {
     int (*f) (int, int);
     int *arr;
-    int start;
     int end;
 } args_t;
+
+static int foo(int acc, int x)
+{
+    return acc + x;
+}
 
 static int reduce_thread(args_t *args)
 {
     int acc = 0;
-
-    for (int *i = args->arr + args->start; i < args->arr + args->end; i++)
+    
+    #pragma omp parallel for reduction (+ : acc)
+    for (int i = 0; i < args->end; i++)
     {
-        acc = args->f(acc, *i);
+        acc = args->f(acc, *((args->arr) + i));
     }
     
     return acc;
@@ -31,10 +37,8 @@ int reduce(int (*f) (int, int), int *arr)
     }
 
     args_t args;
-
     args.f = f;
     args.arr = arr;
-    args.start = 0;
     args.end = *(arr + SIZE_ADRS);
 
     return reduce_thread(&args);
