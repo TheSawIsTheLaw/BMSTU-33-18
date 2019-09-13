6f326cb2e213bf2f7414e01365249ac910dbf291
Elis <sukochava.alis@mail.ru>
2019-09-13 22:19:14 +0300

    fix style
    Former-commit-id: d31bfaf21f451bc023321463cb9430bab40af0de
    Former-commit-id: 497747701bcf4e665b16f10fa626e99e50df6308

--- ARRgame/tests/arrgame_tests_main.c
+++ ARRgame/tests/arrgame_tests_main.c
@@ -1,4 +1,4 @@
-#include "../headers/arrgame_headers_get.h"
+// #include "../headers/arrgame_headers_get.h"
 #include <stdio.h>
 #include <errno.h>
 
@@ -12,82 +12,89 @@ extern int errno;
 
 int get(const int *const array, const int pos)
 {
-	if (*(array - SERVICE_INFO) - pos <= 0 || pos < 0)
-	{
-		errno = POS_ERROR;
-		return ZERO;
-	}
-	return *(array + pos);
+    if (*(array - SERVICE_INFO) - pos <= 0 || pos < 0)
+    {
+        errno = POS_ERROR;
+        return ZERO;
+    }
+    return *(array + pos);
 }
 
 int main(void)
 {
-	// int count = 1;
-	errno = ZERO;
+    int count = 1;
+    errno = ZERO;
 
-	// // Тепличные тесты
-	// int arr1[] = {4, 0, 0, 10, 11, 12, 13};
+    // Тепличные тесты
+    int arr1[] = {4, 0, 0, 10, 11, 12, 13};
 
-	// if (get(arr1 + SERVICE_INFO, 2) != 12)
-	// {
-	// 	printf("Ошибка на тесте: %d\n", count);
-	// 	return ERROR;
-	// }
-	// count++;
+    if (get(arr1 + SERVICE_INFO, 2) != 12)
+    {
+        printf("Ошибка на тесте: %d\n", count);
+        return ERROR;
+    }
+    count++;
 
-	// // Граничные тесты
-	// if (get(arr1 + SERVICE_INFO, 0) != 10)
-	// {
-	// 	printf("Ошибка на тесте: %d\n", count);
-	// 	return ERROR;
-	// }
-	// count++;
+    // Граничные тесты
+    if (get(arr1 + SERVICE_INFO, 0) != 10)
+    {
+        printf("Ошибка на тесте: %d\n", count);
+        return ERROR;
+    }
+    count++;
 
-	// if (get(arr1 + SERVICE_INFO, 3) != 13)
-	// {
-	// 	printf("Ошибка на тесте: %d\n", count);
-	// 	return ERROR;
-	// }
-	// count++;
+    if (get(arr1 + SERVICE_INFO, 3) != 13)
+    {
+        printf("Ошибка на тесте: %d\n", count);
+        return ERROR;
+    }
+    count++;
 
-	// // Запредельные тесты 
-	// int arr2[] = {0};
+    // Запредельные тесты 
+    int arr2[] = {0};
 
-	// if (!get(arr1 + SERVICE_INFO, 999) && errno != POS_ERROR)
-	// {
-	// 	printf("Ошибка на тесте: %d\n", count);	
-	// 	return ERROR;
-	// }
-	// errno = ZERO;
-	// count++;
+    if (!get(arr1 + SERVICE_INFO, 999) && errno != POS_ERROR)
+    {
+        printf("Ошибка на тесте: %d\n", count);    
+        return ERROR;
+    }
+    errno = ZERO;
+    count++;
 
-	// // Пустой массив
-	// if (!get(arr2 + SERVICE_INFO, 0) && errno != POS_ERROR)
-	// {
-	// 	printf("Ошибка на тесте: %d\n", count);	
-	// 	return ERROR;
-	// }
-	// errno = ZERO;
-	// count++;
+    if (!get(arr2 + SERVICE_INFO, 0) && errno != POS_ERROR)
+    {
+        printf("Ошибка на тесте: %d\n", count);    
+        return ERROR;
+    }
+    errno = ZERO;
+    count++;
 
-	// if (!get(arr1 + SERVICE_INFO, -1) && errno != POS_ERROR)
-	// {
-	// 	printf("Ошибка на тесте: %d\n", count);	
-	// 	return ERROR;
-	// }
-	// errno = ZERO;
-	// count++;
+    if (!get(arr1 + SERVICE_INFO, -1) && errno != POS_ERROR)
+    {
+        printf("Ошибка на тесте: %d\n", count);    
+        return ERROR;
+    }
+    errno = ZERO;
+    count++;
 
-	// if (!get(arr1 + SERVICE_INFO, 4) && errno != POS_ERROR)
-	// {
-	// 	printf("Ошибка на тесте: %d\n", count);	
-	// 	return ERROR;
-	// }
-	// errno = ZERO;
-	// count++;
-	int arr3[] = {0, 0, 0};
-	printf("%d %d \n", get(arr3 + SERVICE_INFO, 10), errno);
+    if (!get(arr1 + SERVICE_INFO, 4) && errno != POS_ERROR)
+    {
+        printf("Ошибка на тесте: %d\n", count);    
+        return ERROR;
+    }
+    errno = ZERO;
+    count++;
 
+    int arr3[] = {0, 0, 0};
 
-	return OK;
+    if (!get(arr3 + SERVICE_INFO, 10) && errno != POS_ERROR)
+    {
+        printf("Ошибка на тесте: %d\n", count);    
+        return ERROR;
+    }
+    errno = ZERO;
+    count++;
+    printf("Прошло %d из 9 тестов!\n", count);
+
+    return OK;   
 }
