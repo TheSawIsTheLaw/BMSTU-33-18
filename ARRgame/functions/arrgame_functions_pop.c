eedf2a3fbfa097cc96eb8f5be220f1dda1991b46
Dmitry Kovalev <712-kda18u305@users.noreply.git.iu7.bmstu.ru>
2019-09-13 22:52:23 +0300

    add empty function
    Former-commit-id: ef4f0100fecc6b5f753c53e5fcb870b41d616db3
    Former-commit-id: 4a343a5a52b5dbcfe21bdf168ea90063c64d34ce

---
+++ ARRgame/functions/arrgame_functions_pop.c
@@ -0,0 +1,20 @@
+#include "../headers/arrgame_headers_pop.h"
+
+#include <stdio.h>
+
+typedef pop_args{
+    int *arr;
+    int n;
+};
+
+int pop_base(int *arr, int n)
+{
+    return 0;
+}
+
+int var_pop(pop_args in)
+{
+    int *arr_out = in.arr ? in.arr : 0;
+    int n_out = in.n ? in.n : 3;
+    return pop_base(arr_out, n_out);
+}
