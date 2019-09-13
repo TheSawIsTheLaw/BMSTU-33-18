acb752aefb89a4349356abdb5afd7642ba6ee2ad
Dmitry Kovalev <712-kda18u305@users.noreply.git.iu7.bmstu.ru>
2019-09-13 23:37:01 +0300

    add ;
    Former-commit-id: 0e95e5501a315f73d2bb202290a71f0be43ad060
    Former-commit-id: 92592335f551c114f01cf7d3ada9c451de20d86a

--- ARRgame/headers/arrgame_headers_pop.h
+++ ARRgame/headers/arrgame_headers_pop.h
@@ -1,11 +1,13 @@
 #ifndef _ARRGAME_HEADERS_POP_H_
 #define _ARRGAME_HEADERS_POP_H_
-#include <errno.h>
-#include <stdlib.h>
 
-typedef struct pop_args pop_args;
-int pop_base(int *arr, int n)
-int var_pop(pop_args in)
-#define pop(...) var_f((f_args){__VA_ARGS__});
+typedef struct {
+    int *arr;
+    int n;
+} pop_args;
+
+int pop_base(int *arr, int n);
+int var_pop(pop_args in);
+#define pop(...) var_pop((pop_args){__VA_ARGS__});
 
 #endif
