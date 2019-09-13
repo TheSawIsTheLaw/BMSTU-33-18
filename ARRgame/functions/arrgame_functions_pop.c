e88381a31c4a08bc85f2165e472fd6925c33cfb1
Dmitry Kovalev <712-kda18u305@users.noreply.git.iu7.bmstu.ru>
2019-09-14 00:09:58 +0300

    final
    Former-commit-id: f669b57c7ed4770b54f86c08e08b15de86e30cd3
    Former-commit-id: ae84fbad53fed7677a3e4d27c306a4801f6dc43e

--- ARRgame/functions/arrgame_functions_pop.c
+++ ARRgame/functions/arrgame_functions_pop.c
@@ -1,14 +1,32 @@
 #include "../headers/arrgame_headers_pop.h"
-#include <stdio.h>
+#include <errno.h>
+#define OK 0
+#define POS_ERROR 1
+#define SERVICE_INFO 3
+extern int errno;
 
 int pop_base(int *arr, int n)
 {
-    return 0;
+    if (!arr && (*(arr - SERVICE_INFO) - n <= 0 || n < 0))
+    {
+        errno = POS_ERROR;
+        return 0;
+    }
+    int value = *(arr + n);
+    int kol = *(arr - SERVICE_INFO);
+    for (int i = n; i < kol - 1; i++)
+    {
+        *(arr + i) = *(arr + i + 1);
+    }
+    --*(arr - SERVICE_INFO);
+    return value;
 }
 
 int var_pop(pop_args in)
 {
     int *arr_out = in.arr ? in.arr : 0;
-    int n_out = in.n ? in.n : 3;
+    int n_out = 0;
+    if (arr_out)
+        n_out = in.n ? in.n : *(in.arr - SERVICE_INFO) - 1;
     return pop_base(arr_out, n_out);
 }
