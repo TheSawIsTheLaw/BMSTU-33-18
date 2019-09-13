b5505942fdae4998b4db857b724b62e5808882f2
Dmitry Kovalev <712-kda18u305@users.noreply.git.iu7.bmstu.ru>
2019-09-13 22:27:36 +0300

    add header
    Former-commit-id: 25d20f7da10191f3bfe962ba6fba05a90cb75a39
    Former-commit-id: 495fefd56d162dca10054dc7220b9a4e9b86d844

---
+++ ARRgame/headers/ arrgame_headers_pop.h
@@ -0,0 +1,10 @@
+#ifndef _ARRGAME_HEADERS_POP_H_
+#define _ARRGAME_HEADERS_POP_H_
+#include <errno.h>
+
+typedef struct pop_args pop_args;
+int pop_base(int *arr, int n)
+int var_pop(pop_args in)
+#define pop(...) var_f((f_args){__VA_ARGS__});
+
+#endif
