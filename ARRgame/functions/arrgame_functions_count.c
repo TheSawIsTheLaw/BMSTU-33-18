136536621cb95698158ee4fe08d463fab6640378
Anastasiia <anastasia.namestnik@gmail.com>
2019-09-14 21:15:32 +0300

    Function count() was added
    Former-commit-id: 179c61d2d09c3d886b8bf3486a4e358479f65ef0
    Former-commit-id: 4b77b085c823bc782971344d667c5032b2e9f2ec

--- ARRgame/functions/arrgame_functions_count.c
+++ ARRgame/functions/arrgame_functions_count.c
@@ -14,22 +14,9 @@ int count(const int x, int const* start, int const* end)
     if (start >= end)
         return ERROR_LENGTH;
     
-    int length = start - end + CONSTANT;
-    
     while (start <= end)
         if (*start++ == x)
             count_element++;
    
     return count_element;
-    
-}
-
-int main()
-{
-    int a[6] = {5, 5, 5, 5, 5, 23};
-    int x = 5;
-    
-    printf("%d", count(x, &a[0], &a[5]));
-    
-    return 0;
 }
