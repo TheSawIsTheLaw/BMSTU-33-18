8f8458e5592c9146785b9c98114724aca49cc442
Aksenova Nadezhda <aksen-fliut@mail.ru>
2019-09-15 11:03:49 +0300

    Delete comment marks
    Former-commit-id: eda088e8d63df882b4ebd25d0ca2ba4c24d09a02
    Former-commit-id: 471532fd93ebe2b05f922edbeb063a1c9d17130a

--- ARRgame/functions/arrgame_functions_count.c
+++ ARRgame/functions/arrgame_functions_count.c
@@ -1,8 +1,9 @@
  //Функция для подсчета количества вхождений элемента в массив
 
 
-//#include "../headers/arrgame_headers_count.h"
+#include "../headers/arrgame_headers_count.h"
 #include <stdio.h>
+
 #define ERROR_LENGTH 2
 #define INITIAL 0
 #define CONSTANT 1
