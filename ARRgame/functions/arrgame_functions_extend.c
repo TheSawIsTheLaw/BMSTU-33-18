a64503bc9fc0b09ba30cf48a5e9a4ccebcabda7e
chdv17u353 <kelgam39@gmail.com>
2019-09-16 19:04:15 +0300

    annotation added
    Former-commit-id: 3782f9c8266e6545e76e155cbc654fae4283eb13
    Former-commit-id: aa43d9f400e26c47cfdecfb8305dec8405709473

--- ARRgame/functions/arrgame_functions_extend.c
+++ ARRgame/functions/arrgame_functions_extend.c
@@ -25,14 +25,14 @@
 
 #define EXTEND_REALLOC_FAIL -602
 
-
-/* 
-   При нехватки памяти выделяется новая,
-   возвращает указатель на новую область памяти
-   или NULL и ошибку в errno
- */
 int *extend(int *arra, const int *const arrb);
 
+
+/*
+  simpl_extend - копирует элеметны массива arrb за конец массива arrb
+  @arra - указатель на заголовок расширяемого массива
+  @arrb - указатель на заголовок копируемого массива
+ */
 static void simple_extend(int *arra, const int *const arrb)
 {
     const int ncpy = GET_REC(arrb);
@@ -45,11 +45,14 @@ static void simple_extend(int *arra, const int *const arrb)
     GET_REC(arra) += ncpy;
 }
 
+
+/*
+  safe_realloc - безопасно расширяет область выделенной памяти массива arra
+  @**arra - указатель на заголовок расширяемого массива
+  @desired_size - желаемый новый размер массива
+ */
 static int safe_realloc(int **arra, const size_t desired_size)
 {
-    /* const size_t double_realloc = GET_ALC(*arra) * 2; */
-    /* size_t realloc_size = desired_size > double_realloc ? desired_size : double_realloc; */
-
     const size_t realloc_size = OFFSET_START + desired_size;
 
     int *const temp = realloc((void*) * arra, realloc_size * sizeof(int));
@@ -62,6 +65,17 @@ static int safe_realloc(int **arra, const size_t desired_size)
     return SUCCESS;
 }
 
+
+/*
+  extend - прибавляет элементы массива arrb к массиву arra
+  @*arra - указатель на заголовок расширяемого массива
+  @*arrb - указатель на заголовок прбавляемого массива
+  
+  При расширении адрес расширяемого массива arra может изменится.
+
+  @return возвращает указатель на новую область памяти, 
+  или NULL и код ошибки в errno.
+*/
 int *extend(int *arra, const int *const arrb)
 {
     const int reall_a = GET_REC(arra);
