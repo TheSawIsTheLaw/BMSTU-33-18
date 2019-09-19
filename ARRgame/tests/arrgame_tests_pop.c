a450e4b772dc53737b35b68693ce3d10211113df
Akhmetov Karim <697-akr18u630@users.noreply.git.iu7.bmstu.ru>
2019-09-19 18:09:32 +0300

    Update arrgame_tests_pop.c
    Former-commit-id: 47b48a7ea986917db17250b6a16da8da89ea66fc
    Former-commit-id: 0f965130e3eada74a57be250429a3c50d8d95518

--- ARRgame/tests/arrgame_tests_pop.c
+++ ARRgame/tests/arrgame_tests_pop.c
@@ -53,7 +53,7 @@ int var_pop(pop_args in)
 
 int test(int *array, int *array2, int size2, int pop_number)
 {
-    pop(array2, pop_number);
+    pop(array2[3], pop_number);
     int count_pass = 0;
     int pop_number_check = 0;
     for (int i = 0; i < size2; i++)
