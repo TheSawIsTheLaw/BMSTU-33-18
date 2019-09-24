f045ec8ae109a05b93979ba34922a8b3a31420a7
Артем Саркисов <temasarkisov@MacBook-Pro-Artem.local>
2019-09-24 17:20:57 +0300

    Matrix operations were edited.
    Former-commit-id: 587efe982d9ec0b746863dd64f644bf6d690a558
    Former-commit-id: fd9599532a5888300848b09523d16d5fe03a49df

--- MATRIXgame/functions/matrix_game_insert_el_in_row.c
+++ MATRIXgame/functions/matrix_game_insert_el_in_row.c
@@ -1,4 +1,5 @@
 #include<stdlib.h>
+//#include<stdio.h>
 
 #include "../headers/matrixgame_headers_insert_el_in_row.h"
 #include "../headers/matrixgame_headers_matrix_t.h"
@@ -6,6 +7,33 @@
 
 #define MEM_ERR -808
 #define EL_IND_ERR -809
+
+/*int **matrix_allocate(int **matrix, const int row_num, const int col_num)
+{
+    matrix = (int **) malloc(row_num * sizeof(int *));
+    for (int i = 0; i < row_num; i++)
+        matrix[i] = (int *) malloc(col_num * sizeof(int));
+    
+    return matrix;
+}
+
+void matrix_filling(int **matrix, const int row_num, const int col_num)
+{
+    for (int i = 0; i < row_num; i++)
+        for (int j = 0; j < col_num; j++)
+                matrix[i][j] = 1;
+
+}
+
+void matrix_output(int **matrix, const int row_num, const int col_num)
+{
+    for (int i = 0; i < row_num; i++)
+    {
+        for (int j = 0; j < col_num; j++)
+            printf("%d ", matrix[i][j]);
+        printf("\n");
+    }
+}*/
 
 /*
   Выделяет дополнительную память для каждой строки матрицы.
@@ -18,10 +46,11 @@ int one_pos_shifting(matrix_t *const matrix)
 
     for (int i = 0; i < matrix->rows; i++)
     {
-        (matrix->matrix)[i] = (int *) realloc((matrix->matrix)[i],  matrix->columns * sizeof(int *));
-        if (!(matrix->matrix)[i])
+        *((matrix->matrix) + i) = (int *) realloc((matrix->matrix)[i],  matrix->columns * sizeof(int *));
+        if (!*((matrix->matrix) + i))
             return MEM_ERR;
-        (matrix->matrix)[i][(matrix->columns) - 1] = 0;
+
+        *(*((matrix->matrix) + i) + (matrix->columns - 1)) = 0;
     }
 
     return 0;
@@ -35,14 +64,14 @@ int one_pos_shifting(matrix_t *const matrix)
 
 int insert_el_in_row(matrix_t *const matrix, int index_row, int index_column, int el)
 {
-    if (one_pos_shifting(matrix))
+    if (one_pos_shifting(matrix) != MEM_ERR)
     {
         if (index_row < matrix->rows && index_column < matrix->columns)
         {
             for (int i = (matrix->columns) - 1; i > index_column; i--)
-                (matrix->matrix)[index_row][i] = (matrix->matrix)[index_row][i - 1];
+                *(*((matrix->matrix) + index_row) + i) = *(*((matrix->matrix) + index_row) + (i - 1));
 
-            (matrix->matrix)[index_row][index_column] = el;
+            *(*((matrix->matrix) + index_row) + index_column) = el;
     
             return 0;
         }
@@ -53,3 +82,18 @@ int insert_el_in_row(matrix_t *const matrix, int index_row, int index_column, in
     return MEM_ERR;
 }
 
+/* Test:
+ 
+int main()
+{
+    matrix_t M;
+    M.rows = 3;
+    M.columns = 4;
+
+    M.matrix = matrix_allocate(M.matrix, M.rows, M.columns);
+    matrix_filling(M.matrix, M.rows, M.columns);
+    matrix_output(M.matrix, M.rows, M.columns);
+
+    insert_el_in_row(&M, 2, 2, 8);
+    matrix_output(M.matrix, M.rows, M.columns);
+}*/   
