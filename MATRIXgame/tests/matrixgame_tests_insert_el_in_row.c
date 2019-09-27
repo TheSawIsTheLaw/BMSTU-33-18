501e09fdc7b5452df740bc8b78c27db5e45c40d5
Артем Саркисов <temasarkisov@MacBook-Pro-Artem.local>
2019-09-27 21:43:39 +0300

    Test was added.
    Former-commit-id: 7b3e89de36c89ac430b1de01a0a132769b337eff
    Former-commit-id: 0340a05d006c0ab59585080a3f7594f5c9962bd0

--- MATRIXgame/tests/matrixgame_tests_insert_el_in_row.c
+++ MATRIXgame/tests/matrixgame_tests_insert_el_in_row.c
@@ -4,20 +4,70 @@
 #define PASSED 0
 #define FAILED 1
 
+int matrix_compare(matrix_t tmp_matrix, matrix_t res_matrix)
+{
+    if (tmp_matrix.rows == res_matrix.rows && tmp_matrix.columns == res_matrix.columns)
+    {
+        for (int i = 0; i < res_matrix.rows; i++)
+            for (int j = 0; j < res_matrix.columns; j++)
+                if (tmp_matrix.matrix[i][j] != res_matrix.matrix[i][j])
+                    return 0;
+        
+        return 1; 
+    }
+    
+    return 0;
+}
+
 int matrixgame_insert_el_in_row_test()
 {
-    matrix_t matrix;
+    matrix_t tmp_matrix;
     
-    matrix.rows = 3
-    matrix.columns = 4
-    matrix.matrix = (int **) malloc(matrix.rows * sizeof(int *));
-    for (int i = 0; i < matrix.rows; i++)
-        (matrix.matrix)[i] = (int *) malloc(matrix.columns * sizeof(int));
+    tmp_matrix.rows = 3;
+    tmp_matrix.columns = 3;
+    
+    tmp_matrix.matrix = (int **) malloc(tmp_matrix.rows * sizeof(int *));
+    for (int i = 0; i < tmp_matrix.rows; i++)
+    {
+        (tmp_matrix.matrix)[i] = (int *) malloc(tmp_matrix.columns * sizeof(int));
+        for (int j = 0; j < tmp_matrix.columns; j++)
+            tmp_matrix.matrix[i][j] = 1;
+    }
+
+    matrix_t res_matrix;
+
+    res_matrix.rows = 3;
+    res_matrix.columns = 4;
+    int res_arr[] = { 1, 1, 1, 0, 1, 1, 3, 1, 1, 1, 1, 0 };
+    
+    res_matrix.matrix = (int **) malloc(res_matrix.rows * sizeof(int *));
+    
+    int arr_ind = 0;
+    for (int i = 0; i < res_matrix.rows; i++)
+    {
+        (res_matrix.matrix)[i] = (int *) malloc(res_matrix.columns * sizeof(int));
+        for (int j = 0; j < res_matrix.columns; j++)
+        {
+            res_matrix.matrix[i][j] = res_arr[arr_ind];
+            arr_ind++;
+        }
+    }
+
+    int index_row = 1;
+    int index_column = 2;
+    int el = 3;
+  
+    insert_el_in_row(&tmp_matrix, index_row, index_column, el);
+
+    if (matrix_compare(tmp_matrix, res_matrix))
+        return PASSED;
+  
+    return FAILED;
 }
 
 int main()
 {
-    if (matrixgame_<имя_вашей_функции>_test() != PASSED)
+    if (matrixgame_insert_el_in_row_test() != PASSED)
     {
         return FAILED;
     }
