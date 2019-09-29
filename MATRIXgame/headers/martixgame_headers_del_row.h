d4d97f9360726be5c308c57e0cd8d2d11db5c6f0
Minenko Sergey <serzh.minenko.00@gmail.com>
2019-09-29 21:22:45 +0300

    add header
    Former-commit-id: 480754921bed1dfe54a3e0d7429b145850d209a4
    Former-commit-id: 7562c1454427658a445c2be57419b4da68e856b9

---
+++ MATRIXgame/headers/martixgame_headers_del_row.h
@@ -0,0 +1,8 @@
+#ifndef __MATRIXGAME_HEADERS_DEL_ROW_H__
+#define __MATRIXGAME_HEADERS_DEL_ROW_H__
+
+#include "matrix_headers_matrix_t.h"
+
+matrix_t *get_by_pos(const matrix_t *const edit_matrix, const int index_row)
+
+#endif
