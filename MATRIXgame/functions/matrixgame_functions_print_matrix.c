637881a0901e595ab438c9d57fa9a0889e2acdfe
Sergey <svse>
2019-09-30 00:12:26 +0300

    add print_matrix
    Former-commit-id: c3d27e7455ceddc5f27307ed7b674fcbec34fef5
    Former-commit-id: 84e6654b74e054fcdd1afc066a794cdf7e4997b2

---
+++ MATRIXgame/functions/matrixgame_functions_print_matrix.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../headers/matrixgame_headers_print_matrix.h"
+#include "../headers/matrixgame_headers_matrix_t.h"
+#define ERROR -1
+#define SUCCESS 0
+
+/*
+ Функция для печати матрицы, на вход принимает указатель на структуру, которая содержит все данные о матрице. Дальше идет проверка на количество строк и столбцов, их число должно быть натуральным. В случае ошибки возвращает -1 иначе 0.
+ */
+int print_matrix(const matrix_t *const matrix)
+{
+    if (matrix -> rows < 0)
+        return ERROR;
+    
+    if (matrix -> columns < 0)
+        return ERROR;
+
+    for (int i = 0; i < matrix -> rows; i++)
+    {
+        for (int j = 0; j < matrix -> columns; j++)
+            printf("%d ",  matrix -> matrix[i][j]);
+        printf("\n");
+    }
+    return SUCCESS;
+}
---
+++ MATRIXgame/headers/matrixgame_headers_print_matrix.h
@@ -0,0 +1,9 @@
+#ifndef __MATRIXGAME_HEADERS_PRINT_MATRIX_H__
+#define __MATRIXGAME_HEADERS_PRINT_MATRIX_H__
+
+#include "matrixgame_headers_matrix_t.h"
+
+int print_matrix(const matrix_t *const matrix);
+
+#endif
+
