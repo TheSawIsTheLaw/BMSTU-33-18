1d8497ef7515ac48d5cc0e250ef7d6044d6e8b84
Sergey <svse>
2019-10-01 00:08:27 +0300

    add fix
    Former-commit-id: 5d23a5f9ee385db201aab4b18f33aca209e683fe
    Former-commit-id: cf6ea6e043600aaeca7d6f74004f5a7c71e56126

--- MATRIXgame/functions/matrixgame_functions_print_matrix.c
+++ MATRIXgame/functions/matrixgame_functions_print_matrix.c
@@ -8,19 +8,19 @@
 /*
  Функция для печати матрицы, на вход принимает указатель на структуру, которая содержит все данные о матрице. Дальше идет проверка на количество строк и столбцов, их число должно быть натуральным. В случае ошибки возвращает -1 иначе 0.
  */
-int print_matrix(const matrix_t *const matrix)
+int print_matrix(const matrix_t *const matrix, FILE *file)
 {
     if (matrix -> rows <= 0)
         return ERROR;
-    
+   
     if (matrix -> columns <= 0)
         return ERROR;
-
+ 
     for (int i = 0; i < matrix -> rows; i++)
     {
         for (int j = 0; j < matrix -> columns; j++)
-            printf("%d ",  matrix -> matrix[i][j]);
-        printf("\n");
+            fprintf(file, "%d ",  matrix -> matrix[i][j]);
+        fprintf(file, "\n");
     }
     return SUCCESS;
 }
