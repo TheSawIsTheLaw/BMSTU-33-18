5110fa699e838ceca25ee3c7589b305e6d1ca34d
anm18u107 <aksen-fliut@mail.ru>
2019-10-01 21:59:56 +0300

    Files for tests were added.
    Former-commit-id: bfb9e1b16831a1a6851f2dc07245dd1bdd1a6a92
    Former-commit-id: 283da0169a766929958c5659bc2a723001ad0dc6

---
+++ MATRIXgame/headers/matrixgame_headers_free_matrix.h
@@ -0,0 +1,7 @@
+#ifndef _MATRIXGAME_HEADERS_FREE_MATRIX_H_
+#define _MATRIXGAME_HEADERS_FREE_MATRIX_H_
+#include "matrixgame_headers_matrix_t.h"
+
+int free_matrix(matrix_t *to_free);
+
+#endif
--- MATRIXgame/tests/matrixgame_tests_set_by_pos.c
+++ MATRIXgame/tests/matrixgame_tests_set_by_pos.c
@@ -1,15 +1,43 @@
 #include <stdio.h>
 
 #include "../headers/matrixgame_headers_set_by_pos.h"
+#include "../headers/matrixgame_headers_create_matrix.h"
+#include "../headers/matrixgame_headers_free_matrix.h"
 
 #define INITIAL 0
+
+#define RANGE_ERROR 11
+
 #define PASSED 0
 #define FAILED 1
+
+// Filling matrix with zeros
+void make_zero(matrix_t *const matrix_to_zero)
+{
+    for (int row = 0; row < matrix_to_zero -> rows; ++row)
+    {
+        for (int col = 0; col < matrix_to_zero -> columns; ++col)
+        {
+            *(*(matrix_to_zero -> matrix + row) + col) = INITIAL;
+        }
+    }
+}
 
 int matrixgame_set_by_pos_test()
 {
     int number_of_errors = INITIAL;
   
+    // Test 1: normal work
+    
+    matrix_t matrix_0;
+    create_matrix(&matrix_0, 5, 5);
+    make_zero(&matrix_0);
+    set_by_pos(&matrix_0, 3, 3, 20);
+    if (*(*(matrix_0.matrix + 3) + 3) != 20)
+        number_of_errors ++;
+    
+    free_matrix(&matrix_0);
+    
     if (number_of_errors)
         return FAILED;
     return PASSED;
@@ -19,9 +47,12 @@ int main()
 {
     if (matrixgame_set_by_pos_test() != PASSED)
     {
-    	printf("\nNot all tests passed successfully.");
+    	printf("Not all tests passed successfully.\n");
         return FAILED;
     }
-    printf("\nThere is no failed tests.");
-    return PASSED;
+    else
+    {
+        printf("\nThere is no failed tests.");
+        return PASSED;
+    }
 }
