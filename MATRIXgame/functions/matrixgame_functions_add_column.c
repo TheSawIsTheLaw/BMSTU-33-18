01ce94a7671a0904745db9bfee92b573d9431857
Saburov Sergey <661-ssm18u469@users.noreply.git.iu7.bmstu.ru>
2019-10-02 18:28:06 +0300

    fixes added
    Former-commit-id: c2527ab505c3a35b4cae2d82d334d8529da23cce
    Former-commit-id: 61a8cdf0e5a074201bdc2e1d72a77dcf744ae5d6

---
+++ MATRIXgame/functions/matrixgame_functions_add_column.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+#include "../headers/matrixgame_headers_add_column.h"
+#include "../headers/matrixgame_headers_matrix_t.h"
+
+#define OK 0
+#define ERR 111
+/**
+* \brief Освобождение памяти, если realloc не сработал
+* \param[in] matrix - указатель на структуру, описывающую матрицу
+*/
+static void free_matr(matrix_t *const matrix)
+{
+    for (int i = 0; i < matrix -> rows; i ++)
+    {
+        free(*((matrix->matrix)+ i ));
+    }
+}
+/**
+* \brief Добавление памяти под столбец
+* \param[in] matrix - указатель на структуру, описывающую матрицу
+*/
+static int add_mem_for_vect(matrix_t *const matrix)
+{
+    for (int i = 0; i < matrix->rows;  i++)
+    {
+        (matrix->matrix)[i] =  (int*)realloc(((matrix->matrix)[i]), sizeof(int*)* ((matrix->rows)+ 1));
+        if (*((matrix->matrix)+ i ) == NULL)
+        {
+            free_matr(matrix);
+            return ERR;
+            break;
+
+        }
+    }
+    return OK;
+}
+/**
+* \brief Добавляет столбец
+* \details Выделяет память для переданного столбца
+*
+* \param[in] matrix - указатель на структуру, описывающую матрицу
+* \param[in] vect - указатель на нулевой элемент столбца
+* \return Код ошибки
+* \retval OK Успех
+* \retval ERR Ошибка
+*/
+/*
+ Допущение - количество элементов в инициализированном столбце должно совпадать с количеством рядов матрицы
+*/
+int add_column(matrix_t *const matrix, int vect[])
+{
+    if (matrix->rows <= 0 || matrix->columns <= 0)
+        return ERR;
+    if ((add_mem_for_vect(matrix))!= ERR)
+    {
+        for (int i = 0; i < matrix->rows;  i++)
+        {
+            *(*((matrix->matrix)+ i ) + matrix->columns) = vect[i];
+        }
+        matrix->columns += 1;
+    }
+    else
+        return ERR;
+    return OK;
+}
