ad1cedf02358d2c71b2f485d0a8ee66fd4a0dbb0
Артем Саркисов <temasarkisov@MacBook-Pro-Artem.local>
2019-10-02 21:34:58 +0300

    is_index_correct was added.
    Former-commit-id: 4dba08ead024c843d6e60deb0f264694b731408b
    Former-commit-id: e100fc17c052d17244c8ecc5e1565700e6375a43

--- MATRIXgame/functions/matrixgame_functions_insert_el_in_row.c
+++ MATRIXgame/functions/matrixgame_functions_insert_el_in_row.c
@@ -4,8 +4,22 @@
 #include "../headers/matrixgame_headers_matrix_t.h"
 #include "../headers/matrixgame_errno.h"
 
+#define R_I 1  // Right input.
 #define MEM_ERR -808  // Memory error: array of pointers or values was not allocated right.
 #define EL_IND_ERR -809  // Element index error: element's index is not coorect. 
+
+/*
+  Проверка позиции, на которую будет вставлен элемент
+  (ind >= 0 и ind <= rows/columns).
+*/
+
+int is_index_correct(const int rows && const int columns && const int index_row, const int index_column)
+{
+    if (index_row >= 0 && index_column >= 0 && index_row <= rows && index_column <= columns)
+        return R_I;
+
+    return EL_IND_ERR;
+}
 
 /*
   Выделяет дополнительную память для каждой строки матрицы.
@@ -38,7 +52,7 @@ int insert_el_in_row(matrix_t *const matrix, int index_row, int index_column, in
 {
     if (one_pos_shifting(matrix) != MEM_ERR)
     {
-        if (index_row < matrix->rows && index_column < matrix->columns)
+        if (is_index_correct(matrix->rows, matrix->columns, index_row, index_column) != EL_IND_ERR)
         {
             for (int i = (matrix->columns) - 1; i > index_column; i--)
                 *(*((matrix->matrix) + index_row) + i) = *(*((matrix->matrix) + index_row) + (i - 1));
