cd31fcd219606955696fb3e5693480b71a0b5a71
Kononenko Sergey <sergkononenko@mail.ru>
2019-10-02 22:57:24 +0300

    Resolve errno conflict
    Former-commit-id: f264d58f505116d2a60c2869f7c885aa5c056f3b
    Former-commit-id: 4dc50322a0c46cda63f664c9a0ddda608e068f13

--- MATRIXgame/headers/matrixgame_errno.h
+++ MATRIXgame/headers/matrixgame_errno.h
@@ -1,12 +1,8 @@
-#ifndef  __MATRIXGAME_ERRNO_H__
+#ifndef __MATRIXGAME_ERRNO_H__
 #define __MATRIXGAME_ERRNO_H__
 
 #include <errno.h>
 
-#define SIZE_ERROR 217 
+#define SIZE_ERROR 217
 
-<<<<<<< HEAD
-
-=======
->>>>>>> matrixgame_ci
 #endif
