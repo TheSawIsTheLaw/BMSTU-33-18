4a502261af0b1d473eaa05ec292b03ff590db95e
ptrk <paw.toporkov@gmail.com>
2019-10-03 02:17:56 +0300

    fix func
    Former-commit-id: 1912f80e6f6c3c64d2ff77720e565e5b1af0bb9b
    Former-commit-id: 3f719d371c4fa76603cde4444da0ce96b6101686

--- MATRIXgame/functions/matrixgame_functions_del_row.c
+++ MATRIXgame/functions/matrixgame_functions_del_row.c
@@ -22,16 +22,23 @@ Output data:
 FALSE_POINTER_ERROR or REALLOC_ERROR.
 */
 
-int matr_realloc(int **matrix, const int rows)
+int matr_realloc(matrix_t *matrix)
 {
-    int **tmp = realloc(matrix, sizeof(int*) * rows);
+    int **tmp = realloc(matrix->matrix, sizeof(int*) * matrix->rows);
     if (!tmp)
         return REALLOC_ERROR;
-    matrix = (int**)tmp;
+    matrix->matrix = (int**)tmp;
     tmp = NULL;
     return OK;
 }
 
+void remove_matrix(matrix_t *matrix, const int i_row)
+{
+    for (int i = i_row; i < matrix->rows - 1; i++)
+    {
+        *(matrix->matrix + i) = *(matrix->matrix + i + 1);
+    }
+}
 
 int del_row(matrix_t *edit_matrix, const int index_row)
 {
@@ -40,21 +47,20 @@ int del_row(matrix_t *edit_matrix, const int index_row)
 
     for (int row = 0; row < edit_matrix->rows; ++row)
     {
-        if (!(edit_matrix->matrix + row))
+        if (!(*(edit_matrix->matrix + row)))
         {
             return FALSE_POINTER_ERROR;
         }
     }
 
-    free(*(edit_matrix->matrix + index_row));
-    *(edit_matrix->matrix + index_row) = NULL;
+    remove_matrix(edit_matrix, index_row);
 
+    free(*(edit_matrix->matrix + edit_matrix->rows - 1));
+    *(edit_matrix->matrix + edit_matrix->rows - 1) = NULL;
+    
     edit_matrix->rows -= 1;
 
-    for (int i = index_row; i < edit_matrix->rows - 1; i++)
-        *(edit_matrix->matrix + i) = *(edit_matrix->matrix + i + 1);
-    
-    int error = matr_realloc(edit_matrix->matrix, edit_matrix->rows);
+    int error = matr_realloc(edit_matrix);
     if (error)
         return REALLOC_ERROR;
 
