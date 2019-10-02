42b70d0336229414ab3c67644e9c7c99f48772a8
Sarkisov Artem <635-sas18u476@users.noreply.git.iu7.bmstu.ru>
2019-10-03 01:09:24 +0300

    AAAAAAAAAAAA
    Former-commit-id: 6fdab3b92c5d7f600e14a2bbc168df5fcc24f815
    Former-commit-id: ad1595b4c5f4058750d4b0ecd6b7aa5839e7ef35

--- MATRIXgame/headers/matrixgame_headers_add_row.h
+++ MATRIXgame/headers/matrixgame_headers_add_row.h
@@ -1,6 +1,8 @@
 #ifndef __MATRIXGAME_HEADERS_ADD_ROW_H__
 #define __MATRIXGAME_HEADERS_ADD_ROW_H__
 
+#include "../headers/matrixgame_headers_matrix_t.h"
+
 int my_realloc(matrix_t *matrix);
 
 int add_row(matrix_t *matrix, const int index);
