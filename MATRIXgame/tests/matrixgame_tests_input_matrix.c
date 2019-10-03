e4de0c722c9c981afa4fffceeb6a7094ab762ae0
Namestnik Anastasiia <741-naa18u403@users.noreply.git.iu7.bmstu.ru>
2019-10-04 00:11:05 +0300

    Update matrixgame_tests_input_matrix.c
    Former-commit-id: 9601a5a28210aca97fc77e8a5d9c616f9babed38
    Former-commit-id: 679d6489d5dcb4f1a54da246174f2d5b202125d6

--- MATRIXgame/tests/matrixgame_tests_input_matrix.c
+++ MATRIXgame/tests/matrixgame_tests_input_matrix.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "../headers/matrixgame_headers_input_matrix.h"
-#include "../headers/matrixgame_headers_create_matrix.h"
-#include "../headers/matrixgame_headers_free_matrix.h"
+#include "../headers/matrixgame_headers_matrix_t.h"
 
 #define ROW_ERROR 1
 #define COLUMN_ERROR 2
@@ -12,50 +12,54 @@
 #define ZERO 0
 #define ALL_PASSED 0
 #define FAILED 1
+#define FAILED_POINTER 2
 
-void input_matrix(matrix_t *matrix)
-{
-    for (int i = ZERO; i < matrix -> rows; i++)
-        for (int j = ZERO; j < matrix -> columns; j++)
-            matrix -> matrix[i][j] = i;
-}
 
-int matrixgame_input_matrix_tests()
+int matrixgame_input_matrix_tests(matrix_t *matrix1, matrix_t *matrix2)
 {
     int error = ZERO;
+    int error_null = ZERO;
   
     /*Test 1: NULL pointer*/
-    matrix_t matrix1;
-    create_matrix(&matrix1, 10, 5);
-    matrix1.matrix = NULL;
-    if (input_matrix(&matrix1) == POINT_ERROR)
-        error++;
-    free_matrix(&matrix1);
+    matrix1 = NULL;
+    if (input_matrix(matrix1) == POINT_ERROR)
+        error_null++;
     
     /*Test 2: incorrect input*/
-    matrix_t matrix2;
-    create_matrix(&matrix2, 10, 5);
-    if (input_matrix(&matrix2) != OK)
+    if (input_matrix(matrix2) != OK)
         error++;
-    free_matrix(&matrix2);
     
     /*Test 3: correct input*/
-    matrix_t matrix3;
-    create_matrix(&matrix3, 4, 4);
-    if (input_matrix(&matrix3) != OK)
+    if (input_matrix(matrix2) != OK)
         error++;
-    free_matrix(&matrix3);
-
     
-    if (error)
+    if (error && error_null)
         return FAILED;
+    else if (!error && error_null)
+        return FAILED_POINTER;
     return ALL_PASSED;
 }
 
-
-int main()
+int main(int argc, const char * argv[])
 {
-    if (matrixgame_input_matrix_tests() == ALL_PASSED)
+    matrix_t matrix1;
+    matrix1.rows = 3;
+    matrix1.columns = 3;
+    
+    matrix_t matrix2;
+    matrix2.rows = 3;
+    matrix2.columns = 3;
+    
+    matrix1.matrix = (int**)calloc(matrix1.rows, sizeof(int*));
+    for (int i = 0; i < matrix1.rows; i++)
+        matrix1.matrix[i] = (int*)calloc(matrix1.columns, sizeof(int));
+    
+    matrix2.matrix = (int**)calloc(matrix2.rows, sizeof(int*));
+    for (int i = 0; i < matrix2.rows; i++)
+        matrix2.matrix[i] = (int*)calloc(matrix2.columns, sizeof(int));
+    
+    
+    if (matrixgame_input_matrix_tests(&matrix1, &matrix2) == ALL_PASSED)
     {
         puts("All tests passed (success)");
         return ALL_PASSED;
