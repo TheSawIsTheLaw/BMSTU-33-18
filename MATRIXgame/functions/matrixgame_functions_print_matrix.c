983eb6f58c639a5f5c9c58bf610dff0a778e630e
xAMATEURx <dimakrok@bk.ru>
2019-10-03 21:58:04 +0300

    Doxygen was added. That was c00L...
    Former-commit-id: 298262aec59ccae317aeffd7b348a482f212a225
    Former-commit-id: b01855c82311f2667e88aaed77904a523d09428b

--- MATRIXgame/functions/matrixgame_functions_print_matrix.c
+++ MATRIXgame/functions/matrixgame_functions_print_matrix.c
@@ -1,21 +1,44 @@
+/**
+ * \file matrixgame_functions_print_matrix.c
+ * \brief Функция, выводящая матрицу в файл
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "../headers/matrixgame_headers_print_matrix.h"
 #include "../headers/matrixgame_headers_matrix_t.h"
+
+/**
+ * \def ERROR
+ * \brief Код ошибки: в функцию передана некорректная матрица
+ */
 #define ERROR -1
+/**
+ * \def SUCCESS
+ * \brief Код удачного завершения подпрограммы или программы
+ */
 #define SUCCESS 0
 
-/*
- Функция для печати матрицы, на вход принимает указатель на структуру, которая содержит все данные о матрице. Дальше идет проверка на количество строк и столбцов, их число должно быть натуральным. В случае ошибки возвращает -1 иначе 0.
+/**
+ * \fn int append_row(matrix_t *const matrix)
+ *
+ * \param const matrix_t *const matrix Особо заданная матрица (см. matrixgame_
+ * functions_create_matrix)
+ * \param FILE *file Десриптор файла вывода
+ *
+ * \brief Выводит матрицу в файл
+ *
+ * \return Код ошибки (отличное от нуля число) или
+ * успешного завершения
  */
 int print_matrix(const matrix_t *const matrix, FILE *file)
 {
     if (matrix -> rows <= 0)
         return ERROR;
-   
+
     if (matrix -> columns <= 0)
         return ERROR;
- 
+
     for (int i = 0; i < matrix -> rows; i++)
     {
         for (int j = 0; j < matrix -> columns; j++)
