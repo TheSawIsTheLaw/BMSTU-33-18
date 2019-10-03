b0bf840d9ee9ee88b90fcc724a784cbb951dde35
Namestnik Anastasiia <741-naa18u403@users.noreply.git.iu7.bmstu.ru>
2019-10-03 21:30:01 +0300

    Add new file
    Former-commit-id: ead7fb9f21a2845481f752d80d36bc968009c5af
    Former-commit-id: 07e81b142532b8e7dd9ca9ca292df76852b70e2c

---
+++ MATRIXgame/functions/matrixgame_functions_create_matrix.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+
+#include "../headers/matrixgame_headers_matrix_t.h"
+#include "../headers/matrixgame_headers_create_matrix.h"
+#include "../../ARRgame/headers/arrgame_headers_create.h"
+#include "../../ARRgame/functions/arrgame_functions_create.c"
+
+typedef int mtype;
+
+/**
+ * \brief Освобождает выделнную за диапазаном указателей память.
+ * \param[in] start - Указатель на начало диапазона указателей
+ * \param[in] end - Указатель на конец диапазона указателей.
+ */
+static void clean_up_row_pointers(mtype *const * start, mtype *const *const end)
+{
+    for (; start < end; start++)
+        free(*start);
+}
+
+#define MEM_ALLOC_FAILURE -101  /* Ошибка выделения памяти */
+#define SUCCESS 0               /* успех */
+
+/**
+ * \brief Создаёт матрицу
+ * \details Выделяет память для матрицы, описанной в переданной
+ *             через указатель структуре matrix_t
+ * \param[out] matrix - Указатель на структуру, описывающую матрицу
+ * \param[in] rows - Число столбцов
+ * \param[in] columns - Число строчек
+ * \return Код ошибки
+ * \retval SUCCESS Успех, без ошибки
+ * \retval MEM_ALLOC_FAILURE Ошибка при выделении памяти
+ */
+int create_matrix(matrix_t *const matrix, const int rows, const int columns)
+{
+    void *temp = malloc(sizeof(mtype*) * rows);
+    if (!temp)
+        return MEM_ALLOC_FAILURE;
+
+    matrix->matrix = (mtype**)temp;
+
+    mtype *const *const end = matrix->matrix + rows;
+    for (mtype** cur = matrix->matrix; cur < end; cur++)
+    {
+        temp = create(columns);
+        if (!temp)
+        {
+            clean_up_row_pointers(matrix->matrix, cur);
+            free(matrix->matrix);
+            return MEM_ALLOC_FAILURE;
+        }
+        
+        *cur = (mtype*)temp;
+    }
+
+    matrix->columns = columns;
+    matrix->rows = rows;
+
+    return SUCCESS;
+}
