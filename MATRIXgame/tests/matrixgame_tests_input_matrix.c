ccc78418c3704da4d911c38c590b92b5b74bcb32
Namestnik Anastasiia <741-naa18u403@users.noreply.git.iu7.bmstu.ru>
2019-10-03 20:56:59 +0300

    Tests were added (input_matrix)
    Former-commit-id: c2f73bf6d15fb7182872fc290977f632b0a9d130
    Former-commit-id: 77fdee45f08a05b63a2a63cd855755cae374c073

---
+++ MATRIXgame/tests/matrixgame_tests_input_matrix.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+#include "../headers/matrixgame_headers_input_matrix.h"
+#include "../headers/matrixgame_headers_create_matrix.h"
+#include "../headers/matrixgame_headers_free_matrix.h"
+
+#define ROW_ERROR 1
+#define COLUMN_ERROR 2
+#define POINT_ERROR 3
+#define OK 0
+
+#define ZERO 0
+#define ALL_PASSED 0
+#define FAILED 1
+
+int matrixgame_input_matrix_tests()
+{
+    int error = ZERO;
+  
+    /*Test 1: NULL pointer*/
+    matrix_t matrix1;
+    create_matrix(&matrix1, 10, 5);
+    matrix1.matrix = NULL;
+    if (input_matrix(&matrix1) == POINT_ERROR)
+        error++;
+    free_matrix(&matrix1);
+    
+    /*Test 2: incorrect input*/
+    matrix_t matrix2;
+    create_matrix(&matrix2, 10, 5);
+    if (input_matrix(&matrix2) != OK)
+        error++;
+    free_matrix(&matrix2);
+    
+    /*Test 3: correct input*/
+    matrix_t matrix3;
+    create_matrix(&matrix3, 4, 4);
+    if (input_matrix(&matrix3) != OK)
+        error++;
+    free_matrix(&matrix3);
+
+    
+    if (error)
+        return FAILED;
+    return ALL_PASSED;
+}
+
+
+int main()
+{
+    if (matrixgame_input_matrix_tests() == ALL_PASSED)
+    {
+        puts("All tests passed (success)\n");
+        return ALL_PASSED;
+    }
+    else
+    {
+        puts("Not all the tests passed (failure)\n");
+        return FAILED;
+    }
+}
