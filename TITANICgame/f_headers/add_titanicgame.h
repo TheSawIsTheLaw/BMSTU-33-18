881388c6fe38f2b6f8b1115b497c0031d3c3fbf6
vlados_bmstu <vkrivozubov@list.ru>
2019-10-29 18:37:13 +0300

    FIX: tests for add
    Former-commit-id: 541e1eefc17391774a3292a1e9f0b43f3cfdc66f
    Former-commit-id: 6b7bd9316d288b470c1b4b9bb79ceb78d202d22b

--- TITANICgame/f_headers/add_titanicgame.h
+++ TITANICgame/f_headers/add_titanicgame.h
@@ -3,7 +3,9 @@
 #define _ADD_TITANIC_GAME_H_
 
 #include "../headers/struct.h"
+#include "../f_headers/create_titanicgame.h"
 #include "stdlib.h"
+#include "stdio.h"
 
 int add(node *root, bool choice, node *new_node);
 
