15ece939ef8b293a7251aaf55627b5fb2aa5fcf3
Kononenko Sergey <sergkononenko@mail.ru>
2019-10-30 21:35:40 +0300

    Update struct (don't want to merge)
    Former-commit-id: ff80691b8ecedf3abe878a0d53f6f1aec064d4e8
    Former-commit-id: 95ad994fb3e20130b36394bb81c645721445258d

--- TITANICgame/headers/struct.h
+++ TITANICgame/headers/struct.h
@@ -4,7 +4,11 @@
 
 #define MAX_STRING_LEN 256
 
-typedef enum {FALSE, TRUE} bool;
+typedef enum
+{
+    FALSE,
+    TRUE
+} bool;
 
 /*
 Numeric
@@ -22,29 +26,27 @@ Cabin Cabin number
 Embarked Where the passenger got on the ship 
 (C - Cherbourg, S - Southampton, Q = Queenstown)
 */
-typedef struct 
+typedef struct
 {
-	unsigned int passenger_id; // id пассажира.
-	int survived; // Признак (1 - выжил / 0 - не выжил).
-	int pclass; // Класс пассажира (1, 2, 3).
-	char name[MAX_STRING_LEN]; // Имя.
-	char surname[MAX_STRING_LEN]; // фамилия. 
-	char status; // Mr, Mrs
-	char sex; // w - Девочка, m - Мальчик.   
-	float age; // Возраст.
-	unsigned int siblings_sp; // Кол-во братьев или сестер (в сумме) на корабле.
-	unsigned int parch; // Кол-во детей/родителей на корабле.
-	char ticket[MAX_STRING_LEN]; // Билет.
-	float fare;
-	char cabin[MAX_STRING_LEN];
-	char embarked; // Место (C - Cherbourg, S - Southampton, Q = Queenstown)
-}passenger;
+    unsigned int passenger_id;   // id пассажира.
+    int survived;                // Признак (1 - выжил / 0 - не выжил)
+    int pclass;                  // Класс пассажира (1, 2, 3)
+    char name[MAX_STRING_LEN];   // Имя Фамилия Статус
+    char sex[MAX_STRING_LEN];    // male, female
+    float age;                   // Возраст
+    unsigned int siblings_sp;    // Кол-во братьев или сестер (в сумме) на корабле
+    unsigned int parch;          // Кол-во детей/родителей на корабле
+    char ticket[MAX_STRING_LEN]; // Билет
+    float fare;
+    char cabin[MAX_STRING_LEN];
+    char embarked; // Место (C - Cherbourg, S - Southampton, Q = Queenstown)
+} passenger;
 
 typedef struct node
 {
-  struct node* yes;
-  struct node* no;
-  bool (*decision)(struct passenger);
-}node;
+    struct node *yes;
+    struct node *no;
+    bool (*decision)(passenger);
+} node;
 
 #endif
