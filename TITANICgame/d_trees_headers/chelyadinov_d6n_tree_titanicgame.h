f8d86c72f7a2ade2bfa6bfd90ae8274d943699fe
Chelyadinov Ilya <733-chid18u575@users.noreply.git.iu7.bmstu.ru>
2019-11-09 00:27:50 +0300

    Changed macro

--- TITANICgame/d_trees_headers/chelyadinov_d6n_tree_titanicgame.h
+++ TITANICgame/d_trees_headers/chelyadinov_d6n_tree_titanicgame.h
@@ -1,5 +1,5 @@
-#ifndef TEST_CHEL
-#define TEST_CHEL
+#ifndef __CHELYADINOV_D6N_TREE_TITANICGAME_H__
+#define __CHELYADINOV_D6N_TREE_TITANICGAME_H__
 
     #include "../headers/struct.h"
     node* chelyadinov_d6n_tree_titanicgame();
