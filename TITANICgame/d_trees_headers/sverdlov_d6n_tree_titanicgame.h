185a4f7437026900e1b222d6efbd3de5d8cc99eb
Sergey <svse>
2019-11-08 17:53:21 +0300

    add strategy

---
+++ TITANICgame/d_trees_headers/sverdlov_d6n_tree_titanicgame.h
@@ -0,0 +1,10 @@
+#ifndef __SVERDLOV_D6N_TREE_TITANICGAME_H__
+#define __SVERDLOV_D6N_TREE_TITANICGAME_H__
+
+#include "../headers/struct.h"
+
+node *sverdlov_d6n_tree_titanicgame();
+
+#endif
+
+
---
+++ TITANICgame/decision_trees/sverdlov_d6n_tree_titanicgame.c
@@ -0,0 +1,36 @@
+
+#include "../d_trees_headers/sverdlov_d6n_tree_titanicgame.h"
+#include "../f_headers/add_titanicgame.h"
+#include "../f_headers/create_titanicgame.h"
+
+bool check_age(passenger one)
+{
+    if (a.age > 10)
+        return TRUE;
+    return FALSE;
+}
+
+bool check_sex(passenger one)
+{
+    if (strcmp(a.sex, "female") == 0)
+        return TRUE;
+    return FALSE;
+}
+
+bool check_pclass(passenger one)
+{
+    if (a.pclass == 2)
+        return TRUE;
+    return FALSE;
+}
+
+
+node* sverdlov_d6n_tree_titanicgame()
+{
+    node *root = create(check_age);
+    node *sex = create(check_sex);
+    node *class = create(check_pclass);
+    add(root, TRUE, sex);
+    add(sex, TRUE, class);
+    return root;
+}
