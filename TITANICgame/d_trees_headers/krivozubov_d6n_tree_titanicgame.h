37da64aafe6d0b70c0bcb37f18dd424ba6792b55
vlados_bmstu <vkrivozubov@list.ru>
2019-11-09 00:47:47 +0300

    ADD: number one bullshit strat

---
+++ TITANICgame/d_trees_headers/krivozubov_d6n_tree_titanicgame.h
@@ -0,0 +1,10 @@
+#ifndef _KRIVOZUBOV_D6N_TREE_TITANICGAME_H_H_
+
+#define _KRIVOZUBOV_D6N_TREE_TITANICGAME_H_H_
+
+#include <string.h>
+#include "../headers/struct.h"
+#include "../f_headers/add_titanicgame.h"
+#include "../f_headers/create_titanicgame.h"
+
+#endif 
