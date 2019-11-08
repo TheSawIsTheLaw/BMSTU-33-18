bf93543c2e94fd1c5ebe3afbd41eefd950a7e15e
chdv17u353 <kelgam39@gmail.com>
2019-11-08 20:46:27 +0300

    a strategy

---
+++ TITANICgame/decision_trees/chernenko_d6n_tree_titanicgame.c
@@ -0,0 +1,30 @@
+#include "string.h"
+
+#include "../headers/struct.h"
+#include "../d_trees_headers/chernenko_d6n_tree_titanicgame.h"
+
+bool sex_split(passenger sample)
+{
+    if (strcmp(sample.sex, "female") == 0)
+        return TRUE;
+    else
+        return FALSE;
+}
+
+bool class_split(passenger sample)
+{
+    if (sample.pclass <= 2)
+        return TRUE;
+    else
+        return FALSE;
+}
+
+node *chernenko_d6n_tree_titanicgame()
+{
+    node *root = create(sex_split);
+    node *if_female = create(class_split);
+
+    add(root, TRUE, if_female);
+    
+    return root;
+}
