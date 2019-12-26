286a5911c42f3b2d70bfbe66ada1fec04277c01c
Mike Telnoy <mike12345654321@mail.ru>
2019-12-27 00:39:51 +0300

    add lib true

---
+++ COREgame/c_src_headers/true_headers/true_link.h
@@ -0,0 +1,9 @@
+#ifndef __TRUE_H__
+#define __TRUE_H__
+
+#include <stdio.h>
+
+int true();
+
+#endif
+
---
+++ COREgame/c_test/true_test.c
@@ -0,0 +1,8 @@
+
+#include "../c_src_headers/yes_headers/true_link.h"
+
+int main()
+{
+    printf("Hello\n");
+    return 0;
+}
