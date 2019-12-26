ca9bc34c780859a073b39da354070d86446ee14c
chvd17u353 <kelgam39@gmail.com>
2019-12-26 22:33:05 +0300

    initial rjaka

---
+++ UIgame/include/create_chat_page.h
@@ -0,0 +1,12 @@
+#ifndef _CREATE_CHAT_PAGE_H_
+#define _CREATE_CHAT_PAGE_H_
+
+#include <stdlib.h>
+#include <string.h>
+#include <ui.h>
+
+#define CHAT_TAB_NAME "Чат"
+
+void createChatPage(uiTab *parentTab);
+
+#endif
---
+++ UIgame/src/create_chat_page.c
@@ -0,0 +1,40 @@
+#include "../include/create_chat_page.h"
+
+static uiBox *chtBox;
+static uiEntry *chtEntry;
+
+static void on_anime_press(uiButton *but, void *data)
+{
+    (void)but;
+    (void)data;
+    
+    uiBox *newBox = uiNewHorizontalBox();
+    uiLabel *newLabel = uiNewLabel(uiEntryText(chtEntry));
+
+    uiBoxAppend(newBox, uiControl(newLabel), 1);
+    uiBoxAppend(chtBox, uiControl(newBox), 0);
+}
+
+void createChatPage(uiTab *parentTab)
+{
+    uiBox *verBox = uiNewVerticalBox();
+    uiBox *conBox = uiNewHorizontalBox();
+    chtBox = uiNewVerticalBox();
+
+    uiBoxSetPadded(verBox, 1);
+    uiBoxSetPadded(conBox, 1);
+    uiBoxSetPadded(chtBox, 1);
+
+    uiBoxAppend(verBox, uiControl(chtBox), 1);
+    uiBoxAppend(verBox, uiControl(conBox), 0);
+
+    uiButton *sndButton = uiNewButton("anime");
+    chtEntry = uiNewEntry();
+
+    uiButtonOnClicked(sndButton, on_anime_press, NULL);
+
+    uiBoxAppend(conBox, uiControl(sndButton), 1);
+    uiBoxAppend(conBox, uiControl(chtEntry), 1);
+    
+    uiTabAppend(parentTab, CHAT_TAB_NAME, uiControl(verBox));
+}
