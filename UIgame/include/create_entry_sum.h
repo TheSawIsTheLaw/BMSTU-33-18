95d980c9715e1c5490c6726c88dbf7d796364f43
ptrk <paw.toporkov@gmail.com>
2019-12-27 18:48:51 +0300

    add entry sum

--- UIgame/include/create_entry_sum.h
+++ UIgame/include/create_entry_sum.h
@@ -1,4 +1,4 @@
-#ifdef CREATE_ENTRY_SUM
+#ifndef CREATE_ENTRY_SUM
 #define CREATE_ENTRY_SUM
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,4 +7,5 @@
 #include "../include/create_main_page.h"
 
 uiEntry* create_entry_sum(void);
+
 #endif
--- UIgame/src/create_operations_page.c
+++ UIgame/src/create_operations_page.c
@@ -1,5 +1,9 @@
 #include "../include/create_operations_page.h"
 
+uiGroup *group;
+uiEntry *entryForm;
+uiBox *hbox, *vbox;
+
 int onClosing1(uiWindow *w, void *data)
 {
     uiControlDestroy(uiControl(w));
@@ -15,10 +19,29 @@ int onShouldQuit1(void *data)
 
 void createOperationsPage(uiWindow *operationWnd)
 {
-    operationWnd = uiNewWindow("OleneffBank: Account", 400, 400, 0);
+    operationWnd = uiNewWindow("OleneffBank: Operations", 500, 500, 0);
     uiWindowSetMargined(operationWnd, 0);
     uiWindowOnClosing(operationWnd, onClosing1, NULL);
     uiOnShouldQuit(onShouldQuit1, operationWnd);
     uiControlShow(uiControl(operationWnd));
+
+    group = uiNewGroup("Operations");
+	uiGroupSetMargined(group, 20);
+
+    hbox = uiNewHorizontalBox();
+	uiBoxSetPadded(hbox, 1);
+    uiWindowSetChild(operationWnd, uiControl(hbox));
+    uiBoxAppend(hbox, uiControl(group), 1);
+
+    entryForm = create_entry_sum();
+    uiGroupSetChild(group, uiControl(entryForm));
+
+    vbox = uiNewVerticalBox();
+	uiBoxSetPadded(vbox, 1);
+	uiGroupSetChild(group, uiControl(vbox));
+
+    uiBoxAppend(vbox, uiControl(entryForm), 0);
+
+    uiControl(hbox);
 }
 
