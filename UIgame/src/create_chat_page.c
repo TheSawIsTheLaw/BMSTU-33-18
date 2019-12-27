b14197449420d5d564171a05c79ce20a3aa4efe1
chvd17u353 <kelgam39@gmail.com>
2019-12-28 01:11:35 +0300

    style

--- UIgame/src/create_chat_page.c
+++ UIgame/src/create_chat_page.c
@@ -1,39 +1,79 @@
+#include <string.h>
+
 #include "../include/create_chat_page.h"
 
+#define MSG_COUNT 10
+#define MSG_LEN 256
+
 static uiBox *chtBox;
+static uiBox *msgBoxes[MSG_COUNT] = {0};
+static char msgBuffer[MSG_COUNT][MSG_LEN] = {{0}};
+int msgCount = 0;
+static uiLabel *msgLabels[MSG_COUNT] = {0};
+
 static uiEntry *chtEntry;
+
+#define INVERSE(x) MSG_COUNT - x - 1
+
+void msgMovUp()
+{
+    for (int i = 0; i < MSG_COUNT; i++)
+        memmove(msgBuffer[i], msgBuffer[i + 1], MSG_LEN);
+}
+
+void msgRedraw()
+{
+    for (int i = 0; i < MSG_COUNT; i++)
+        uiLabelSetText(msgLabels[i], msgBuffer[i]);
+}
+
+void msgAppend(char *msg)
+{
+    msgMovUp();    
+
+    memmove(msgBuffer[MSG_COUNT - 1], msg, MSG_LEN);
+    
+    msgRedraw();
+}
+
+char *msgCreate(char *sndr, char *txt)
+{
+    struct passwd *p = getpwuid(getuid());
+    time_t now = time(0);
+    char *msg = ctime(&now);
+    
+    strcat(msg, p->pw_name);
+    msg[strlen(msg)] = '\n';
+
+    msg = ctime(&now);
+    strcat(msg, sndr);
+    strcat(msg, txt);
+
+    return msg;
+}
 
 static void onSend(uiButton *but, void *data)
 {
     (void)but;
     (void)data;
+    
+    msgAppend(msgCreate("Bot Alexey\n", strcat(uiEntryText(chtEntry), "\n")));
+    msgAppend(msgCreate("Bot Anton\n", "MR умер\n"));
+}
 
-    struct passwd *p = getpwuid(getuid());
-    time_t now = time(0);
-    char *msg = ctime(&now);
-    strcat(msg, p->pw_name);
-    msg[strlen(msg)] = '\n';
-    strcat(msg, uiEntryText(chtEntry));
+static void newBoxes(uiGrid *chtGrid)
+{
+    for (int i = 0; i < MSG_COUNT; i++)
+    {
+        int left = i % 2 == 0 ? 1 : 0;
 
-    uiBox *newBox = uiNewHorizontalBox();
-    uiLabel *newLabel = uiNewLabel(msg);
+        msgBoxes[i] = uiNewHorizontalBox();
+        msgLabels[i] = uiNewLabel("");
 
-    uiBoxSetPadded(newBox, 1);
-
-    uiBoxAppend(newBox, uiControl(newLabel), 1);
-    uiBoxAppend(chtBox, uiControl(newBox), 0);
-
-    msg = ctime(&now);
-    strcat(msg, "Bot Anton\n");
-    strcat(msg, "Извините! Merge request'ы сегодня больше не принимаем!");
-
-    newBox = uiNewHorizontalBox();
-    newLabel = uiNewLabel(msg);
-
-    uiBoxSetPadded(newBox, 1);
-
-    uiBoxAppend(newBox, uiControl(newLabel), 1);
-    uiBoxAppend(chtBox, uiControl(newBox), 0);
+        uiBoxAppend(msgBoxes[i], uiControl(msgLabels[i]), 1);
+        uiGridAppend(chtGrid, uiControl(msgBoxes[i]), left, i, 1, 1,
+                     1, uiAlignStart, 1, uiAlignCenter);
+    }
 }
 
 void createChatPage(uiTab *parentTab)
@@ -54,8 +94,13 @@ void createChatPage(uiTab *parentTab)
 
     uiButtonOnClicked(sndButton, onSend, NULL);
 
+    uiGrid *newGrid = uiNewGrid();
+
+    newBoxes(newGrid);
+
     uiBoxAppend(conBox, uiControl(sndButton), 1);
     uiBoxAppend(conBox, uiControl(chtEntry), 1);
+    uiBoxAppend(chtBox, uiControl(newGrid), 1);
 
     uiTabAppend(parentTab, CHAT_TAB_NAME, uiControl(verBox));
 }
