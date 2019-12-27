e6310982d2ddb07f4898d20b80b39baee2ef213e
Saburov Sergey <661-ssm18u469@users.noreply.git.iu7.bmstu.ru>
2019-12-28 00:43:32 +0300

    Upload New File

---
+++ UIgame/include/uiNewFindEntry.h
@@ -0,0 +1,12 @@
+#ifndef UINEWFINDENTRY
+#define UINEWFINDENTRY
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../include/ui.h"
+#include "../include/create_main_page.h"
+
+
+uiEntry* uiNewFindEntry(void);
+
+#endif
