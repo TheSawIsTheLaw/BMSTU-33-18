cff80f7f8b99546a1cf27dae0eee92e36e3425dc
Pavel Perestoronin <p_perestoronin@mail.ru>
2019-12-28 03:04:17 +0300

    ADD: lib work

--- COREgame/c_src_headers/nice_headers/nice_upload.h
+++ COREgame/c_src_headers/nice_headers/nice_upload.h
@@ -1,6 +1,12 @@
 #ifndef _NICE_UPLOAD_H_
 #define _NICE_UPLOAD_H_
 
-// here would be upload...
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+typedef int (*nice_fp)(char **, int *, int *);
+
+nice_fp get_nice();
 
 #endif
---
+++ COREgame/c_test_headers/nice_test.h
@@ -0,0 +1,6 @@
+#ifndef _READLINK_TEST_H_
+#define _READLINK_TEST_H_
+
+int nice_test();
+
+#endif
